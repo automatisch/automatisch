fb3de58ccf32023652c9e285d0bd4ab1df62b702
Faruk AYDIN <omerfaruk26@gmail.com>
2021-10-02 13:36:57 +0200

    chore: Add backend package

---
+++ packages/backend/__tests__/backend.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const backend = require('..');
+
+describe('backend', () => {
+    it('needs tests');
+});
---
+++ packages/backend/lib/backend.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = backend;
+
+function backend() {
+    // TODO
+}
