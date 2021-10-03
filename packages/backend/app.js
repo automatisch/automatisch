0ffa19025047493110e95a976b5139857e21d1c0
Faruk AYDIN <omerfaruk26@gmail.com>
2021-10-03 18:03:50 +0200

    chore: Setup express app

---
+++ packages/backend/app.js
@@ -0,0 +1,31 @@
+var createError = require('http-errors');
+var express = require('express');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
