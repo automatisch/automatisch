c0f022a1b09233e1def6d18866dfebcca41e8c4c
Faruk AYDIN <omerfaruk26@gmail.com>
2021-10-04 21:40:17 +0200

    chore: Adjust cors options

--- packages/backend/src/config/app.ts
+++ packages/backend/src/config/app.ts
@@ -2,12 +2,18 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 type AppConfig = {
+  host: string,
+  protocol: string
   port: string,
+  corsPort: string,
   appEnv: string,
 }
 
 const appConfig: AppConfig = {
+  host: process.env.HOST || 'localhost',
+  protocol: process.env.PROTOCOL || 'http',
   port: process.env.PORT || '3000',
+  corsPort: process.env.CORS_PORT || '3001',
   appEnv: process.env.APP_ENV || 'development',
 }
 
