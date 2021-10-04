fc9bcc5c14697770b3006a9fa5a895110b1a9a01
Ali BARIN <ali.barin53@gmail.com>
2021-10-04 20:25:18 +0200

    chore: add app config with GQL URL and PORT 3001

--- packages/web/src/graphql/client.ts
+++ packages/web/src/graphql/client.ts
@@ -1,6 +1,10 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import appConfig from 'config/app';
 
 const cache = new InMemoryCache();
-const client = new ApolloClient({ cache: cache });
+const client = new ApolloClient({
+  uri: appConfig.graphqlUrl,
+  cache
+});
 
 export default client;
