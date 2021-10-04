7d98333a8a8a51f4631fd99bb3b183f0a28d0530
Faruk AYDIN <omerfaruk26@gmail.com>
2021-10-04 19:42:23 +0200

    chore: Integrate graphql to the express app

--- packages/backend/src/helpers/morgan.ts
+++ packages/backend/src/helpers/morgan.ts
@@ -1,13 +1,19 @@
 import morgan, { StreamOptions } from 'morgan';
-
+import { Request } from 'express';
 import logger from './logger';
 
 const stream: StreamOptions = {
-  write: (message) => logger.http(message.substring(0, message.lastIndexOf('\n')))
+  write: (message) => logger.http(message.substring(0, message.lastIndexOf("\n")))
 };
 
+const registerGraphQLToken = () => {
+  morgan.token("graphql-query", (req: Request) => `GraphQL ${req.body.query}`);
+};
+
+registerGraphQLToken();
+
 const morganMiddleware = morgan(
-  ":method :url :status :res[content-length] - :response-time ms",
+  ":method :url :status :res[content-length] - :response-time ms\n:graphql-query",
   { stream }
 );
 
