908f156a456d58319258a9d69269440cc7950cc5
Faruk AYDIN <omerfaruk26@gmail.com>
2021-10-05 18:37:57 +0200

    chore: Add database create & drop commands

--- packages/backend/src/config/app.ts
+++ packages/backend/src/config/app.ts
@@ -7,6 +7,11 @@ type AppConfig = {
   port: string,
   corsPort: string,
   appEnv: string,
+  postgresDatabase: string,
+  postgresPort: number,
+  postgresHost: string,
+  postgresUsername: string,
+  postgresPassword: string
 }
 
 const appConfig: AppConfig = {
@@ -15,6 +20,11 @@ const appConfig: AppConfig = {
   port: process.env.PORT || '3000',
   corsPort: process.env.CORS_PORT || '3001',
   appEnv: process.env.APP_ENV || 'development',
+  postgresDatabase: process.env.POSTGRES_DATABASE || 'automatisch_development',
+  postgresPort: parseInt(process.env.POSTGRES_PORT) || 5432,
+  postgresHost: process.env.POSTGRES_HOST || 'localhost',
+  postgresUsername: process.env.POSTGRES_USERNAME || 'automatish_development_user',
+  postgresPassword: process.env.POSTGRESS_PASSWORD,
 }
 
 export default appConfig;
