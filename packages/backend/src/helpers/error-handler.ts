754a145587c0ca3566b8a80684fdd67598351cf6
Faruk AYDIN <omerfaruk26@gmail.com>
2021-10-05 18:34:40 +0200

    chore: Extract error handler and graphql instance to seperate files

---
+++ packages/backend/src/helpers/error-handler.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from 'express';
+import logger from './logger';
+
+const errorHandler = (err: any, req: Request, res: Response, _next: NextFunction) => {
+  if(err.message === 'Not Found') {
+    res.status(404).end()
+  } else {
+    logger.error(err.message)
+    res.status(500).end()
+  }
+}
+
+export default errorHandler;
