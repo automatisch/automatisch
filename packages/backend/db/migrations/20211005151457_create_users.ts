095772844ded54e8819099d72dea19ff319f5c11
Faruk AYDIN <omerfaruk26@gmail.com>
2021-10-06 23:37:45 +0200

    feat: Create user model

---
+++ packages/backend/db/migrations/20211005151457_create_users.ts
@@ -0,0 +1,17 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id');
+    table.string('email').unique().notNullable();
+    table.string('password').notNullable();
+
+    table.timestamps(true, true);
+  });
+
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('users');
+}
+
