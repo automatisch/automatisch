2fbbdd60f4a34156a79d62b2c6b40911b51da79a
Ali BARIN <ali.barin53@gmail.com>
2021-10-07 01:10:19 +0200

    feat: Add dummy pages

---
+++ packages/web/src/components/PageTitle/index.tsx
@@ -0,0 +1,15 @@
+import Typography from '@mui/material/Typography';
+
+type PageTitleProps = {
+  children: React.ReactNode;
+};
+
+export default function PageTitle(props: PageTitleProps) {
+  const { children } = props;
+
+  return (
+    <Typography variant="h4">
+      {children}
+    </Typography>
+  );
+}
---
+++ packages/web/src/pages/Explore/index.tsx
@@ -0,0 +1,3 @@
+export default function Explore() {
+  return (<>Explore</>);
+};
