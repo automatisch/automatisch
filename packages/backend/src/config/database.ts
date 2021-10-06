f7887f94182068456a79b35dfd8571cc21135901
Faruk AYDIN <omerfaruk26@gmail.com>
2021-10-06 23:39:02 +0200

    chore: Extract knex model integration to separate file

--- packages/backend/src/config/database.ts
+++ packages/backend/src/config/database.ts
@@ -1,6 +1,6 @@
+import { Model } from 'objection';
 import knexInstance from 'knex';
 import knexConfig from '../../knexfile';
 
 const knex = knexInstance(knexConfig)
-
-export default knex;
+Model.knex(knex)
