4dbbf378444ecd6e87cd4db19eeea8d9606dbdf1
Faruk AYDIN <omerfaruk26@gmail.com>
2021-10-09 12:39:18 +0200

    chore: Build graphQL query type dynamically

--- packages/backend/src/graphql/graphql-schema.ts
+++ packages/backend/src/graphql/graphql-schema.ts
@@ -1,9 +1,21 @@
-import { buildSchema } from 'graphql';
+import { GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLList } from 'graphql';
+import getApps from './queries/get-apps';
 
-const graphQLSchema = buildSchema(`
-  type Query {
-    getApps(name: String): [String!]
+const queryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    getApps: {
+      type: GraphQLList(GraphQLString),
+      args: {
+        name: { type: GraphQLString }
+      },
+      resolve: (_, { name }) => getApps(name)
+    }
   }
-`);
+});
+
+var graphQLSchema = new GraphQLSchema({
+  query: queryType,
+});
 
 export default graphQLSchema;
