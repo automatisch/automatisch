4c6f41c6792992cdacd51118c010bf2c48f73b44
Faruk AYDIN <omerfaruk26@gmail.com>
2021-10-09 13:31:44 +0200

    feat: Implement getApp graphQL query

--- packages/backend/src/graphql/graphql-schema.ts
+++ packages/backend/src/graphql/graphql-schema.ts
@@ -1,5 +1,7 @@
-import { GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLList } from 'graphql';
+import { GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLList, GraphQLNonNull } from 'graphql';
 import getApps from './queries/get-apps';
+import getApp from './queries/get-app';
+import appType from './types/app';
 
 const queryType = new GraphQLObjectType({
   name: 'Query',
@@ -10,7 +12,14 @@ const queryType = new GraphQLObjectType({
         name: { type: GraphQLString }
       },
       resolve: (_, { name }) => getApps(name)
-    }
+    },
+    getApp: {
+      type: appType,
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve: (_, { name }) => getApp(name)
+    },
   }
 });
 
