568ce6a4e023f4f69ec740947b7a431a4d9eaa4a
Faruk AYDIN <omerfaruk26@gmail.com>
2021-10-11 16:48:52 +0200

    chore: Implement sample user creation script

---
+++ packages/backend/bin/database/create-user.ts
@@ -0,0 +1,15 @@
+import User from '../../src/models/user';
+import '../../src/config/database';
+import logger from '../../src/helpers/logger';
+
+const userParams = {
+  email: 'user@automatisch.com',
+  password: 'sample',
+};
+
+async function createUser() {
+  const user = await User.query().insertAndFetch(userParams);
+  logger.info(`User has been saved: ${user.email}`);
+}
+
+createUser();
