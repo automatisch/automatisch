7f17420ae0c82ad8819935fbc7eaa40535834004
Faruk AYDIN <omerfaruk26@gmail.com>
2021-10-13 16:19:13 +0200

    chore: Expose authentication steps to the client

---
+++ packages/backend/src/graphql/types/authentication-step.ts
@@ -0,0 +1,34 @@
+import { GraphQLObjectType, GraphQLString, GraphQLList, GraphQLInt } from 'graphql';
+
+const authenticationStepType = new GraphQLObjectType({
+  name: 'authenticationStep',
+  fields: {
+    step: { type: GraphQLInt },
+    type: { type: GraphQLString },
+    name: { type: GraphQLString },
+    fields: { 
+      type: GraphQLList(
+        new GraphQLObjectType({
+          name: 'authenticationStepFields',
+          fields: {
+            name: { type: GraphQLString },
+            value: { type: GraphQLString },
+            fields: {  
+              type: GraphQLList(
+                new GraphQLObjectType({
+                  name: 'twitterAuthenticationStepFields',
+                  fields: {
+                    name: { type: GraphQLString },
+                    value: { type: GraphQLString }
+                  }
+                })
+              )
+            },
+          }
+        }),
+      )
+    }
+  }
+})
+
+export default authenticationStepType;
