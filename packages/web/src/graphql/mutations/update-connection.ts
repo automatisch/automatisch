48ba4565e486f1c95b5b0f1f46b5c9f86e7e1dc1
Ali BARIN <ali.barin53@gmail.com>
2021-10-17 16:08:05 +0200

    refactor: Update credential wording with connection

---
+++ packages/web/src/graphql/mutations/update-connection.ts
@@ -0,0 +1,15 @@
+import { gql } from '@apollo/client';
+
+export const UPDATE_CONNECTION = gql`
+  mutation UpdateConnection($id: String!, $data: twitterCredentialInput!) {
+    updateConnection(id: $id, data: $data) {
+      id
+      key
+      verified
+      data {
+        consumerKey
+        consumerSecret
+      }
+    }
+  }
+`;
