258b523042aed2d4156a51d3ed2b1d6923dbf7f1
Faruk AYDIN <omerfaruk26@gmail.com>
2021-10-18 17:05:36 +0200

    feat: Implement getAppConnections graphQL query

--- packages/backend/src/graphql/root-query.ts
+++ packages/backend/src/graphql/root-query.ts
@@ -2,13 +2,15 @@ import { GraphQLObjectType } from 'graphql';
 import getApps from './queries/get-apps';
 import getApp from './queries/get-app';
 import getConnectedApps from './queries/get-connected-apps';
+import getAppConnections from './queries/get-app-connections';
 
 const rootQuery = new GraphQLObjectType({
   name: 'Query',
   fields: {
     getApps,
     getApp,
-    getConnectedApps
+    getConnectedApps,
+    getAppConnections
   }
 });
 
