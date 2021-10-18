f39b2c80116abc1d71fcb35ffc321eca0e7d5232
Faruk AYDIN <omerfaruk26@gmail.com>
2021-10-18 17:29:53 +0200

    feat: Implement deleteConnection mutation

---
+++ packages/backend/src/graphql/mutations/delete-connection.ts
@@ -0,0 +1,26 @@
+import { GraphQLString, GraphQLNonNull, GraphQLBoolean } from 'graphql';
+import Connection from '../../models/connection';
+import RequestWithCurrentUser from '../../types/express/request-with-current-user';
+
+type Params = {
+  id: string,
+  data: object
+}
+const deleteConnectionResolver = async (params: Params, req: RequestWithCurrentUser) => {
+  await Connection.query().delete().findOne({
+    user_id: req.currentUser.id,
+    id: params.id
+  })
+
+  return;
+}
+
+const deleteConnection = {
+  type: GraphQLBoolean,
+  args: {
+    id: { type: GraphQLNonNull(GraphQLString) }
+  },
+  resolve: (_: any, params: Params, req: RequestWithCurrentUser) => deleteConnectionResolver(params, req)
+};
+
+export default deleteConnection;
--- packages/backend/src/graphql/root-mutation.ts
+++ packages/backend/src/graphql/root-mutation.ts
@@ -2,13 +2,15 @@ import { GraphQLObjectType } from 'graphql';
 import createConnection from './mutations/create-connection';
 import createAuthLink from './mutations/create-auth-link';
 import updateConnection from './mutations/update-connection';
+import deleteConnection from './mutations/delete-connection';
 
 const rootMutation = new GraphQLObjectType({
   name: 'Mutation',
   fields: {
-    createConnection: createConnection,
-    createAuthLink: createAuthLink,
-    updateConnection: updateConnection
+    createConnection,
+    createAuthLink,
+    updateConnection,
+    deleteConnection
   }
 });
 
