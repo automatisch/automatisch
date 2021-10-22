2d349e9ce2bd8227641328c56ddcef14c0bd999f
Faruk AYDIN <omerfaruk26@gmail.com>
2021-10-22 15:55:25 +0200

    feat: Introduce reset and verify connection mutations

--- packages/web/src/graphql/mutations/index.ts
+++ packages/web/src/graphql/mutations/index.ts
@@ -1,5 +1,7 @@
 import { CREATE_CONNECTION } from './create-connection';
 import { UPDATE_CONNECTION } from './update-connection';
+import { VERIFY_CONNECTION } from './verify-connection';
+import { RESET_CONNECTION } from './reset-connection';
 import { DELETE_CONNECTION } from './delete-connection';
 import { CREATE_AUTH_LINK } from './create-auth-link';
 
@@ -10,6 +12,8 @@ type Mutations = {
 const mutations: Mutations = {
   createConnection: CREATE_CONNECTION,
   updateConnection: UPDATE_CONNECTION,
+  verifyConnection: VERIFY_CONNECTION,
+  resetConnection: RESET_CONNECTION,
   deleteConnection: DELETE_CONNECTION,
   createAuthLink: CREATE_AUTH_LINK,
 };
