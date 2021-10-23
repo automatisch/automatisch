e9ba654b250c74e727945e550d885e6600cf6b94
Ali BARIN <ali.barin53@gmail.com>
2021-10-23 18:53:48 +0200

    feat: introduce snackbars for GQL errors

--- packages/web/src/graphql/client.ts
+++ packages/web/src/graphql/client.ts
@@ -1,10 +1,23 @@
 import { ApolloClient } from '@apollo/client';
 import cache from './cache';
+import createLink from './link';
 import appConfig from 'config/app';
 
+type CreateClientOptions = {
+  onError?: (message: string) => void;
+};
+
 const client = new ApolloClient({
-  uri: appConfig.graphqlUrl,
-  cache
+  cache,
+  link: createLink({ uri: appConfig.graphqlUrl })
 });
+
+export function setLink({ onError }: CreateClientOptions) {
+  const link = createLink({ uri: appConfig.graphqlUrl, onError });
+
+  client.setLink(link);
+
+  return client;
+};
 
 export default client;
