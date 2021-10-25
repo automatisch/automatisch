19a1537d7892f2467ed70c0e2242c282a61eb014
Faruk AYDIN <omerfaruk26@gmail.com>
2021-10-25 16:49:03 +0200

    feat: Implement SMTP app connection

---
+++ packages/backend/src/apps/smtp/index.ts
@@ -0,0 +1,40 @@
+import nodemailer from 'nodemailer';
+import App from '../../models/app';
+
+export default class SMTP {
+  client: any
+  connectionData: any
+  appData: any
+
+  constructor(connectionData: any) {
+    this.client = nodemailer.createTransport({
+      host: connectionData.host,
+      port: connectionData.port,
+      secure: connectionData.useTls,
+      auth: {
+        user: connectionData.username,
+        pass: connectionData.password,
+      },
+    });
+
+    this.connectionData = connectionData;
+    this.appData = App.findOneByKey('smtp');
+  }
+
+  async verifyCredentials() {
+    await this.client.verify()
+
+    return {
+      screenName: this.connectionData.username
+    }
+  }
+
+  async isStillVerified() {
+    try {
+      await this.client.verify()
+      return true;
+    } catch(error) {
+      return false
+    }
+  }
+}
