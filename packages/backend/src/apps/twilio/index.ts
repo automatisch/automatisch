9f9f9da1f1351d4ddfb2ea08c767a70e76b89f3f
Ali BARIN <ali.barin53@gmail.com>
2021-10-27 20:04:08 +0200

    feat: introduce twilio auth

---
+++ packages/backend/src/apps/twilio/index.ts
@@ -0,0 +1,37 @@
+import TwilioApi from 'twilio';
+import App from '../../models/app';
+
+export default class Twilio {
+  client: any
+  connectionData: any
+  appData: any
+
+  constructor(connectionData: any) {
+    this.client = TwilioApi(connectionData.accountSid, connectionData.authToken);
+
+    this.connectionData = connectionData;
+    this.appData = App.findOneByKey('twilio');
+  }
+
+  async verifyCredentials() {
+    await this.verify();
+
+    return {
+      screenName: this.connectionData.accountSid
+    }
+  }
+
+  async verify() {
+    try {
+      await this.client.keys.list({ limit: 1 })
+      return true;
+    } catch(error) {
+      // Test credentials throw HTTP 403 and thus, we need to have an exception.
+      return error?.status === 403;
+    }
+  }
+
+  async isStillVerified() {
+    return this.verify();
+  }
+}
