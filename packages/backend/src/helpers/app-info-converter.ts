f0e25ed9f3320d537eabf7bc06e37275809282bc
Faruk AYDIN <omerfaruk26@gmail.com>
2021-11-24 00:16:29 +0100

    fix: Use computed raw data instead of raw app data for convertion

--- packages/backend/src/helpers/app-info-converter.ts
+++ packages/backend/src/helpers/app-info-converter.ts
@@ -3,7 +3,7 @@ import appConfig from '../config/app';
 
 const appInfoConverter = (rawAppData: string) => {
   let computedRawData = rawAppData.replace('{BASE_URL}', appConfig.baseUrl);
-  computedRawData = rawAppData.replace('{WEB_APP_URL}', appConfig.webAppUrl);
+  computedRawData = computedRawData.replace('{WEB_APP_URL}', appConfig.webAppUrl);
 
   const computedJSONData: AppInfoType = JSON.parse(computedRawData)
   return computedJSONData;
