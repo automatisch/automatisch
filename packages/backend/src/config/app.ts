77ab83b43b958d80f304a04c049e7f75df8b9d1d
Faruk AYDIN <omerfaruk26@gmail.com>
2021-11-23 11:16:57 +0100

    chore: Use web app url instead of cors protocol, port and host

--- packages/backend/src/config/app.ts
+++ packages/backend/src/config/app.ts
@@ -5,8 +5,7 @@ type AppConfig = {
   host: string,
   protocol: string
   port: string,
-  corsPort: string,
-  corsProtocol: string,
+  webAppUrl: string,
   appEnv: string,
   postgresDatabase: string,
   postgresPort: number,
@@ -14,15 +13,13 @@ type AppConfig = {
   postgresUsername: string,
   postgresPassword: string,
   baseUrl?: string
-  webAppUrl?: string
 }
 
 const appConfig: AppConfig = {
   host: process.env.HOST || 'localhost',
   protocol: process.env.PROTOCOL || 'http',
   port: process.env.PORT || '3000',
-  corsPort: process.env.CORS_PORT || '3001',
-  corsProtocol: process.env.CORS_PROTOCOL || 'http',
+  webAppUrl: process.env.webAppUrl || 'https://localhost:3001',
   appEnv: process.env.APP_ENV || 'development',
   postgresDatabase: process.env.POSTGRES_DATABASE || 'automatisch_development',
   postgresPort: parseInt(process.env.POSTGRES_PORT) || 5432,
@@ -30,9 +27,6 @@ const appConfig: AppConfig = {
   postgresUsername: process.env.POSTGRES_USERNAME || 'automatish_development_user',
   postgresPassword: process.env.POSTGRESS_PASSWORD,
 }
-
-const webAppUrl = `${appConfig.corsProtocol}://${appConfig.host}:${appConfig.corsPort}`;
-appConfig.webAppUrl = webAppUrl;
 
 const baseUrl = `${appConfig.protocol}://${appConfig.host}:${appConfig.port}`;
 appConfig.baseUrl = baseUrl;
