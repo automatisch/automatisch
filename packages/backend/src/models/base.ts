9169994c0b3742567d4d0a36a74671fa939c325a
Faruk AYDIN <omerfaruk26@gmail.com>
2021-11-24 23:37:25 +0100

    chore: Implement not found error for queries without a result

--- packages/backend/src/models/base.ts
+++ packages/backend/src/models/base.ts
@@ -1,4 +1,5 @@
-import { Model, QueryContext, ModelOptions, snakeCaseMappers, ColumnNameMappers } from 'objection';
+import { Model, snakeCaseMappers } from 'objection';
+import type { Constructor, TransactionOrKnex, QueryBuilderType, QueryContext, ModelOptions, ColumnNameMappers } from 'objection';
 
 class Base extends Model {
   createdAt!: string;
@@ -7,6 +8,13 @@ class Base extends Model {
   static get columnNameMappers(): ColumnNameMappers {
     return snakeCaseMappers();
   }
+
+  static query<M extends Model>(
+    this: Constructor<M>,
+    trxOrKnex?: TransactionOrKnex
+  ): QueryBuilderType<M> {
+    return super.query(trxOrKnex).throwIfNotFound() as QueryBuilderType<M>;
+  };
 
   async $beforeInsert(queryContext: QueryContext) {
     await super.$beforeInsert(queryContext);
