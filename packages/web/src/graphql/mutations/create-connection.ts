3260bf8215766fc28bf3caff40f1041e2f60b580
Ali BARIN <ali.barin53@gmail.com>
2021-11-24 19:25:54 +0100

    fix: introduce AvailableAppsEnumType in createConnection

--- packages/web/src/graphql/mutations/create-connection.ts
+++ packages/web/src/graphql/mutations/create-connection.ts
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const CREATE_CONNECTION = gql`
-  mutation CreateConnection($key: String!, $data: JSONObject!) {
+  mutation CreateConnection($key: AvailableAppsEnumType!, $data: JSONObject!) {
     createConnection(key: $key, data: $data) {
       id
       key
