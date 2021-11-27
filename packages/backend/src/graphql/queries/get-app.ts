4dc16ebda51aec9fd90bdd86ae4c32c4becb052b
Faruk AYDIN <omerfaruk26@gmail.com>
2021-11-27 13:39:51 +0100

    chore: Use available apps enum type in queries and mutations

--- packages/backend/src/graphql/queries/get-app.ts
+++ packages/backend/src/graphql/queries/get-app.ts
@@ -1,19 +1,15 @@
-import { GraphQLString, GraphQLNonNull } from 'graphql';
+import { GraphQLNonNull } from 'graphql';
 import App from '../../models/app';
 import appType from '../types/app';
 import RequestWithCurrentUser from '../../types/express/request-with-current-user';
 import Connection from '../../models/connection';
-import connectionType from '../types/connection';
+import availableAppsEnumType from '../types/available-apps-enum-type';
 
 type Params = {
   key: string
 }
 
 const getAppResolver = async (params: Params, req: RequestWithCurrentUser) => {
-  if(!params.key) {
-    throw new Error('No key provided.')
-  }
-
   const app = App.findOneByKey(params.key);
 
   if (req.currentUser?.id) {
@@ -32,7 +28,7 @@ const getAppResolver = async (params: Params, req: RequestWithCurrentUser) => {
 const getApp = {
   type: appType,
   args: {
-    key: { type: GraphQLNonNull(GraphQLString) },
+    key: { type: GraphQLNonNull(availableAppsEnumType) },
   },
   resolve: (_: any, params: Params, req: RequestWithCurrentUser) => getAppResolver(params, req)
 }
