02cfb979bc6a27909d26d2acdf33abb22547eaf7
Faruk AYDIN <omerfaruk26@gmail.com>
2022-01-08 17:15:39 +0300

    chore: Add active column to flows

---
+++ packages/backend/src/db/migrations/20220108141045_add_active_column_to_flows.ts
@@ -0,0 +1,13 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.table('flows', (table) => {
+    table.boolean('active').defaultTo(false)
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.table('flows', (table) => {
+    table.dropColumn('active');
+  });
+}
