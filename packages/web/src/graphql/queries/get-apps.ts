3925de13a7323272af1bdefb3b2f8ebbab3dd8e7
Ali BARIN <ali.barin53@gmail.com>
2022-01-28 16:17:23 +0100

    fix: do not update step on initial render

--- packages/web/src/graphql/queries/get-apps.ts
+++ packages/web/src/graphql/queries/get-apps.ts
@@ -3,24 +3,70 @@ import { gql } from '@apollo/client';
 export const GET_APPS = gql`
   query GetApps($name: String) {
     getApps(name: $name) {
-      key
       name
+      key
       iconUrl
       docUrl
       primaryColor
+      connectionCount
       fields {
         key
         label
         type
         required
         readOnly
+        value
         placeholder
         description
         docUrl
         clickToCopy
       }
+      authenticationSteps {
+        step
+        type
+        name
+        arguments {
+          name
+          value
+          type
+          properties {
+            name
+            value
+          }
+        }
+      }
+      reconnectionSteps {
+        step
+        type
+        name
+        arguments {
+          name
+          value
+          type
+          properties {
+            name
+            value
+          }
+        }
+      }
       connections {
         id
+      }
+      triggers {
+        name
+        key
+        description
+        subSteps {
+          name
+        }
+      }
+      actions {
+        name
+        key
+        description
+        subSteps {
+          name
+        }
       }
     }
   }
