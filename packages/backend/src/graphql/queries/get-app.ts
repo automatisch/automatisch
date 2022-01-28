92b4dd318914b0abfab71c1a58b7e5616b722022
Faruk AYDIN <omerfaruk26@gmail.com>
2022-01-28 21:08:49 +0300

    refactor: Refactor queries by using related query from objectionjs

--- packages/backend/src/graphql/queries/get-app.ts
+++ packages/backend/src/graphql/queries/get-app.ts
@@ -2,19 +2,21 @@ import { GraphQLNonNull } from 'graphql';
 import App from '../../models/app';
 import appType from '../types/app';
 import RequestWithCurrentUser from '../../types/express/request-with-current-user';
-import Connection from '../../models/connection';
 import availableAppsEnumType from '../types/available-apps-enum-type';
 
 type Params = {
-  key: string
-}
+  key: string;
+};
 
 const getAppResolver = async (params: Params, req: RequestWithCurrentUser) => {
   const app = App.findOneByKey(params.key);
 
-  if (req.currentUser?.id) {
-    const connections = await Connection.query()
-      .where({ user_id: req.currentUser.id, key: params.key });
+  if (req.currentUser) {
+    const connections = await req.currentUser
+      .$relatedQuery('connections')
+      .where({
+        key: params.key,
+      });
 
     return {
       ...app,
@@ -23,14 +25,15 @@ const getAppResolver = async (params: Params, req: RequestWithCurrentUser) => {
   }
 
   return app;
-}
+};
 
 const getApp = {
   type: appType,
   args: {
     key: { type: GraphQLNonNull(availableAppsEnumType) },
   },
-  resolve: (_: any, params: Params, req: RequestWithCurrentUser) => getAppResolver(params, req)
-}
+  resolve: (_: any, params: Params, req: RequestWithCurrentUser) =>
+    getAppResolver(params, req),
+};
 
 export default getApp;
--- packages/backend/src/graphql/queries/get-flows.ts
+++ packages/backend/src/graphql/queries/get-flows.ts
@@ -1,19 +1,21 @@
 import { GraphQLList } from 'graphql';
-import Flow from '../../models/flow';
 import RequestWithCurrentUser from '../../types/express/request-with-current-user';
 import flowType from '../types/flow';
 
-const getFlowsResolver = async (req: RequestWithCurrentUser): Promise<any[]> => {
-  const flows = await Flow.query()
-    .withGraphJoined('[steps.[connection]]')
-    .where({'flows.user_id': req.currentUser.id});
+const getFlowsResolver = async (
+  req: RequestWithCurrentUser
+): Promise<any[]> => {
+  const flows = await req.currentUser
+    .$relatedQuery('flows')
+    .withGraphJoined('[steps.[connection]]');
 
   return flows;
-}
+};
 
 const getFlows = {
   type: GraphQLList(flowType),
-  resolve: (_: any, _params: any, req: RequestWithCurrentUser) => getFlowsResolver(req)
-}
+  resolve: (_: any, _params: any, req: RequestWithCurrentUser) =>
+    getFlowsResolver(req),
+};
 
 export default getFlows;
