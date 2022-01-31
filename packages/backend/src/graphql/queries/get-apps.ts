6c0ba197c66f16dd4710f3947e7eb3540d9eac3a
Faruk AYDIN <omerfaruk26@gmail.com>
2022-01-31 23:14:49 +0300

    feat: Implement onlyWithTriggers flag for getApps graphQL query

--- packages/backend/src/graphql/queries/get-apps.ts
+++ packages/backend/src/graphql/queries/get-apps.ts
@@ -1,21 +1,29 @@
-import { GraphQLString, GraphQLList } from 'graphql';
+import { GraphQLString, GraphQLList, GraphQLBoolean } from 'graphql';
 import appType from '../types/app';
 import App from '../../models/app';
 
 type Params = {
-  name: string
-}
+  name: string;
+  onlyWithTriggers: boolean;
+};
 
 const getAppsResolver = (params: Params) => {
-  return App.findAll(params.name)
-}
+  const apps = App.findAll(params.name);
+
+  if (params.onlyWithTriggers) {
+    return apps.filter((app: any) => app.triggers?.length);
+  }
+
+  return apps;
+};
 
 const getApps = {
   type: GraphQLList(appType),
   args: {
-    name: { type: GraphQLString }
+    name: { type: GraphQLString },
+    onlyWithTriggers: { type: GraphQLBoolean },
   },
-  resolve: (_: any, params: Params) => getAppsResolver(params)
-}
+  resolve: (_: any, params: Params) => getAppsResolver(params),
+};
 
 export default getApps;
