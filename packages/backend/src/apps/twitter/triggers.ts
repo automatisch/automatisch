7726e1dec183496762f25aabba0ad5237ebf4e23
Faruk AYDIN <omerfaruk26@gmail.com>
2022-03-12 14:27:15 +0300

    feat: Implement search tweet trigger step for twitter

--- packages/backend/src/apps/twitter/triggers.ts
+++ packages/backend/src/apps/twitter/triggers.ts
@@ -1,10 +1,13 @@
-import MyTweet from './triggers/my-tweet';
 import { IJSONObject } from '@automatisch/types';
+import MyTweet from './triggers/my-tweet';
+import SearchTweet from './triggers/search-tweet';
 
 export default class Triggers {
   myTweet: MyTweet;
+  searchTweet: SearchTweet;
 
-  constructor(connectionData: IJSONObject) {
+  constructor(connectionData: IJSONObject, parameters: IJSONObject) {
     this.myTweet = new MyTweet(connectionData);
+    this.searchTweet = new SearchTweet(connectionData, parameters);
   }
 }
---
+++ packages/backend/src/apps/twitter/triggers/search-tweet.ts
@@ -0,0 +1,27 @@
+import TwitterApi, { TwitterApiTokens } from 'twitter-api-v2';
+import { IJSONObject } from '@automatisch/types';
+
+export default class SearchTweet {
+  client: TwitterApi;
+  parameters: IJSONObject;
+
+  constructor(connectionData: IJSONObject, parameters: IJSONObject) {
+    this.client = new TwitterApi({
+      appKey: connectionData.consumerKey,
+      appSecret: connectionData.consumerSecret,
+      accessToken: connectionData.accessToken,
+      accessSecret: connectionData.accessSecret,
+    } as TwitterApiTokens);
+
+    this.parameters = parameters;
+  }
+
+  async run() {
+    const response = await this.client.v2.get('tweets/search/recent', {
+      query: this.parameters.searchTerm as string,
+      max_results: 100,
+    });
+
+    return response.data;
+  }
+}
