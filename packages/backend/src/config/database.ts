b6de2a66a07234dec78d906a5bd3c750f3fbe66b
Ali BARIN <ali.barin53@gmail.com>
2022-04-03 16:09:35 +0200

    feat: log error when DB connection is refused

--- packages/backend/src/config/database.ts
+++ packages/backend/src/config/database.ts
@@ -1,7 +1,19 @@
+import process from 'process';
 import knex from 'knex';
 import type { Knex } from 'knex';
 import knexConfig from '../../knexfile';
+import logger from '../helpers/logger';
 
 const knexInstance: Knex = knex(knexConfig);
+
+const CONNECTION_REFUSED = 'ECONNREFUSED';
+
+knexInstance.raw('SELECT 1')
+  .catch((err) => {
+    if (err.code === CONNECTION_REFUSED) {
+      logger.error('Make sure you have installed PostgreSQL and it is running.', err);
+      process.exit();
+    }
+  });
 
 export default knexInstance;
