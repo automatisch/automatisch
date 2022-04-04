568bcbf3950c651c9fc1a148e03255ca3b7a3c70
Ali BARIN <ali.barin53@gmail.com>
2022-04-04 22:03:20 +0200

    chore: pull non-dev logs to info level

--- packages/backend/src/helpers/logger.ts
+++ packages/backend/src/helpers/logger.ts
@@ -10,7 +10,7 @@ const levels = {
 }
 
 const level = () => {
-  return appConfig.appEnv === 'development' ? 'debug' : 'warn'
+  return appConfig.appEnv === 'development' ? 'debug' : 'info'
 }
 
 const colors = {
