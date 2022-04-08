dc5cd76cb6bcebe313f7ba0d33505654186457c8
Ali BARIN <ali.barin53@gmail.com>
2022-04-08 21:19:57 +0200

    fix(backend): correct app favicon path

--- packages/backend/src/helpers/app-assets-handler.ts
+++ packages/backend/src/helpers/app-assets-handler.ts
@@ -5,9 +5,19 @@ const appAssetsHandler = async (app: Application) => {
   const appNames = App.list;
 
   appNames.forEach(appName => {
+    const svgPath = `${__dirname}/../apps/${appName}/assets/favicon.svg`;
+    const staticFileHandlerOptions = {
+      /**
+       * Disabling fallthrough is important to respond with HTTP 404.
+       * Otherwise, web app might be served.
+       */
+      fallthrough: false,
+    };
+    const staticFileHandler = express.static(svgPath, staticFileHandlerOptions);
+
     app.use(
       `/apps/${appName}/assets/favicon.svg`,
-      express.static(`src/apps/${appName}/assets/favicon.svg`)
+      staticFileHandler
     )
   })
 }
