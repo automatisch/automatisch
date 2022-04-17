b65fa88f16263031d94c768eb7d04e52e5b0039e
Ali BARIN <ali.barin53@gmail.com>
2022-04-17 20:03:09 +0200

    feat(web): replace JSONViewer with react-json-tree

--- packages/web/src/components/JSONViewer/index.tsx
+++ packages/web/src/components/JSONViewer/index.tsx
@@ -1,31 +1,44 @@
 import * as React from 'react';
-import '@alenaksu/json-viewer';
-
+import { JSONTree } from 'react-json-tree';
 import type { IJSONObject } from '@automatisch/types';
-import { jsonViewerStyles } from './style';
 
 type JSONViewerProps = {
   data: IJSONObject;
 }
+
+const theme = {
+  scheme: 'inspector',
+  author: 'Alexander Kuznetsov (alexkuz@gmail.com)',
+  base00: '#181818',
+  base01: '#282828',
+  base02: '#383838',
+  base03: '#585858',
+  base04: '#b8b8b8',
+  base05: '#d8d8d8',
+  base06: '#e8e8e8',
+  base07: '#FFFFFF',
+  base08: '#E92F28',
+  base09: '#dc9656',
+  base0A: '#f7ca88',
+  base0B: '#65AD00',
+  base0C: '#86c1b9',
+  base0D: '#347BD9',
+  base0E: '#EC31C0',
+  base0F: '#a16946',
+};
+
 function JSONViewer(props: JSONViewerProps) {
   const { data } = props;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const viewerRef = React.useRef<any>(null);
-
-  React.useEffect(() => {
-    if (viewerRef.current){
-      viewerRef.current.data = data;
-    }
-  }, [data]);
 
   return (
-    <div>
-      {jsonViewerStyles}
-
-      <json-viewer ref={viewerRef} />
-    </div>
+    <JSONTree
+      hideRoot
+      data={data}
+      shouldExpandNode={() => true}
+      invertTheme={true}
+      theme={theme}
+    />
   );
 }
-
 
 export default JSONViewer;
