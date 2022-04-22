bdfdb1f228fcd48c844f37eb2c0ffe9e44bdf62d
Ali BARIN <ali.barin53@gmail.com>
2022-04-22 17:31:28 +0200

    feat: add new milestone trigger in GitHub

---
+++ packages/backend/src/apps/github/triggers/new-milestone.ts
@@ -0,0 +1,63 @@
+import { Octokit } from 'octokit';
+import { DateTime } from 'luxon';
+import { IJSONObject } from '@automatisch/types';
+
+export default class NewMilestone {
+  client?: Octokit;
+  repoOwner?: string;
+  repo?: string;
+
+  constructor(connectionData: IJSONObject, parameters: IJSONObject) {
+    if (connectionData.accessToken) {
+      this.client = new Octokit({
+        auth: connectionData.accessToken as string,
+      });
+    }
+
+    if (parameters?.repo) {
+      const [owner, repo] = (parameters.repo as string).split('/');
+
+      this.repoOwner = owner;
+      this.repo = repo;
+    }
+  }
+
+  get options() {
+    return {
+      owner: this.repoOwner,
+      repo: this.repo,
+      state: 'open' as const,
+    };
+  }
+
+  async run(startTime: Date) {
+    const iterator = await this.client.paginate.iterator(this.client.rest.issues.listMilestones, this.options);
+    const newMilestones = [];
+
+    const startTimeDateObject = DateTime.fromJSDate(startTime);
+
+    milestoneIterator:
+    for await (const { data: milestones } of iterator) {
+      for (const milestone of milestones) {
+        const createdAtDateObject = DateTime.fromISO(milestone.created_at);
+
+        if (createdAtDateObject < startTimeDateObject) {
+          break milestoneIterator;
+        }
+
+        newMilestones.push(milestone);
+      }
+    }
+
+    return newMilestones;
+  }
+
+  async testRun() {
+    const options = {
+      ...this.options,
+      per_page: 1,
+    };
+
+    return (await this.client.rest.issues.listMilestones(options)).data;
+  }
+}
