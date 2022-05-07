8386c9f2e41b347af2cdd73802858e9f244149dc
Ali BARIN <ali.barin53@gmail.com>
2022-05-07 17:11:26 +0200

    feat: add every month trigger in schedule integration

--- packages/backend/src/apps/schedule/triggers.ts
+++ packages/backend/src/apps/schedule/triggers.ts
@@ -2,15 +2,18 @@ import { IJSONObject } from '@automatisch/types';
 import EveryHour from './triggers/every-hour';
 import EveryDay from './triggers/every-day';
 import EveryWeek from './triggers/every-week';
+import EveryMonth from './triggers/every-month';
 
 export default class Triggers {
   everyHour: EveryHour;
   everyDay: EveryDay;
   everyWeek: EveryWeek;
+  everyMonth: EveryMonth;
 
   constructor(connectionData: IJSONObject, parameters: IJSONObject) {
     this.everyHour = new EveryHour(parameters);
     this.everyDay = new EveryDay(parameters);
     this.everyWeek = new EveryWeek(parameters);
+    this.everyMonth = new EveryMonth(parameters);
   }
 }
--- packages/backend/src/apps/schedule/utils.ts
+++ packages/backend/src/apps/schedule/utils.ts
@@ -7,6 +7,7 @@ export const cronTimes = {
   everyDayAt: (hour: number) => `0 ${hour} * * *`,
   everyDayExcludingWeekendsAt: (hour: number) => `0 ${hour} * * 1-5`,
   everyWeekOnAndAt: (weekday: number, hour: number) => `0 ${hour} * * ${weekday}`,
+  everyMonthOnAndAt: (day: number, hour: number) => `0 ${hour} ${day} * *`,
 };
 
 export function getNextCronDateTime(cronString: string) {
