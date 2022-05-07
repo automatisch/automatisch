d065d119c4b15656a3385d57e9ebd739296f41e7
Ali BARIN <ali.barin53@gmail.com>
2022-05-07 15:18:32 +0200

    feat: add every week trigger in schedule integration

--- packages/backend/src/apps/schedule/triggers.ts
+++ packages/backend/src/apps/schedule/triggers.ts
@@ -1,13 +1,16 @@
 import { IJSONObject } from '@automatisch/types';
 import EveryHour from './triggers/every-hour';
 import EveryDay from './triggers/every-day';
+import EveryWeek from './triggers/every-week';
 
 export default class Triggers {
   everyHour: EveryHour;
   everyDay: EveryDay;
+  everyWeek: EveryWeek;
 
   constructor(connectionData: IJSONObject, parameters: IJSONObject) {
     this.everyHour = new EveryHour(parameters);
     this.everyDay = new EveryDay(parameters);
+    this.everyWeek = new EveryWeek(parameters);
   }
 }
--- packages/backend/src/apps/schedule/utils.ts
+++ packages/backend/src/apps/schedule/utils.ts
@@ -6,6 +6,7 @@ export const cronTimes = {
   everyHourExcludingWeekends: '0 * * * 1-5',
   everyDayAt: (hour: number) => `0 ${hour} * * *`,
   everyDayExcludingWeekendsAt: (hour: number) => `0 ${hour} * * 1-5`,
+  everyWeekOnAndAt: (weekday: number, hour: number) => `0 ${hour} * * ${weekday}`,
 };
 
 export function getNextCronDateTime(cronString: string) {
