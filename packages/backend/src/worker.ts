7787436fd42566b593bc71e25ac1c57a0b308959
Faruk AYDIN <omerfaruk26@gmail.com>
2022-05-12 23:17:05 +0200

    fix: Add uncaught exceptions log

--- packages/backend/src/worker.ts
+++ packages/backend/src/worker.ts
@@ -1,2 +1,11 @@
 import './config/orm';
 import './workers/processor';
+
+process
+  .on('unhandledRejection', (reason, p) => {
+    console.error(reason, 'Unhandled Rejection at Promise', p);
+  })
+  .on('uncaughtException', (err) => {
+    console.error(err, 'Uncaught Exception thrown');
+    process.exit(1);
+  });
