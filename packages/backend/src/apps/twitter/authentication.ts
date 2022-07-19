997775e54bce90194b9707e5e16a477f369a2ab7
Faruk AYDIN <omerfaruk26@gmail.com>
2022-07-19 16:45:47 +0300

    refactor: Adjust twitter authentication to use http client

--- packages/backend/src/apps/twitter/authentication.ts
+++ packages/backend/src/apps/twitter/authentication.ts
@@ -4,25 +4,37 @@ import type {
   IField,
   IJSONObject,
 } from '@automatisch/types';
-import TwitterApi, { TwitterApiTokens } from 'twitter-api-v2';
+import HttpClient from '../../helpers/http-client';
+import OAuth from 'oauth-1.0a';
+import crypto from 'crypto';
+import { URLSearchParams } from 'url';
 
 export default class Authentication implements IAuthentication {
   appData: IApp;
   connectionData: IJSONObject;
-  client: TwitterApi;
+
+  client: HttpClient;
+  oauthClient: OAuth;
+
+  static baseUrl = 'https://api.twitter.com';
 
   constructor(appData: IApp, connectionData: IJSONObject) {
     this.appData = appData;
     this.connectionData = connectionData;
-
-    const clientParams = {
-      appKey: connectionData.consumerKey,
-      appSecret: connectionData.consumerSecret,
-      accessToken: connectionData.accessToken,
-      accessSecret: connectionData.accessSecret,
-    } as TwitterApiTokens;
-
-    this.client = new TwitterApi(clientParams);
+    this.client = new HttpClient({ baseURL: Authentication.baseUrl });
+    this.oauthClient = new OAuth({
+      consumer: {
+        key: this.connectionData.consumerKey as string,
+        secret: this.connectionData.consumerSecret as string,
+      },
+      signature_method: 'HMAC-SHA1',
+      hash_function(base_string, key) {
+        return crypto
+          .createHmac('sha1', key)
+          .update(base_string)
+          .digest('base64');
+      },
+    });
   }
 
   async createAuthData() {
@@ -31,35 +43,83 @@ export default class Authentication implements IAuthentication {
     );
     const callbackUrl = appFields.value;
 
-    const authLink = await this.client.generateAuthLink(callbackUrl);
-
-    return {
-      url: authLink.url,
-      accessToken: authLink.oauth_token,
-      accessSecret: authLink.oauth_token_secret,
+    const requestData = {
+      url: `${Authentication.baseUrl}/oauth/request_token`,
+      method: 'POST',
+      data: { oauth_callback: callbackUrl },
     };
+
+    const authHeader = this.oauthClient.toHeader(
+      this.oauthClient.authorize(requestData)
+    );
+
+    try {
+      const response = await this.client.post(`/oauth/request_token`, null, {
+        headers: { ...authHeader },
+      });
+
+      const responseData = Object.fromEntries(
+        new URLSearchParams(response.data)
+      );
+
+      return {
+        url: `${Authentication.baseUrl}/oauth/authorize?oauth_token=${responseData.oauth_token}`,
+        accessToken: responseData.oauth_token,
+        accessSecret: responseData.oauth_token_secret,
+      };
+    } catch (error) {
+      const errorMessages = error.response.data.errors
+        .map((error: IJSONObject) => error.message)
+        .join(' ');
+
+      throw new Error(
+        `Error occured while verifying credentials: ${errorMessages}`
+      );
+    }
   }
 
   async verifyCredentials() {
-    const verifiedCredentials = await this.client.login(
-      this.connectionData.oauthVerifier as string
+    const verifiedCredentials = await this.client.post(
+      `/oauth/access_token?oauth_verifier=${this.connectionData.oauthVerifier}&oauth_token=${this.connectionData.accessToken}`,
+      null
+    );
+
+    const responseData = Object.fromEntries(
+      new URLSearchParams(verifiedCredentials.data)
     );
 
     return {
       consumerKey: this.connectionData.consumerKey,
       consumerSecret: this.connectionData.consumerSecret,
-      accessToken: verifiedCredentials.accessToken,
-      accessSecret: verifiedCredentials.accessSecret,
-      userId: verifiedCredentials.userId,
-      screenName: verifiedCredentials.screenName,
+      accessToken: responseData.oauth_token,
+      accessSecret: responseData.oauth_token_secret,
+      userId: responseData.user_id,
+      screenName: responseData.screen_name,
     };
   }
 
   async isStillVerified() {
     try {
-      await this.client.currentUser();
+      const token = {
+        key: this.connectionData.accessToken as string,
+        secret: this.connectionData.accessSecret as string,
+      };
+
+      const requestData = {
+        url: `${Authentication.baseUrl}/1.1/account/verify_credentials.json`,
+        method: 'GET',
+      };
+
+      const authHeader = this.oauthClient.toHeader(
+        this.oauthClient.authorize(requestData, token)
+      );
+
+      await this.client.get(`/1.1/account/verify_credentials.json`, {
+        headers: { ...authHeader },
+      });
+
       return true;
-    } catch {
+    } catch (error) {
       return false;
     }
   }
