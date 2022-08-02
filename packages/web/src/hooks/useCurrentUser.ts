ae8f701e5cc1e79bd7db0a2f0fc0459338d14f71
Ali BARIN <ali.barin53@gmail.com>
2022-08-02 20:31:39 +0200

    fix: use cache-and-network fetchPolicy for consistency

--- packages/web/src/hooks/useCurrentUser.ts
+++ packages/web/src/hooks/useCurrentUser.ts
@@ -4,7 +4,7 @@ import { IUser } from '@automatisch/types';
 import { GET_CURRENT_USER } from 'graphql/queries/get-current-user';
 
 export default function useCurrentUser(): IUser {
-  const { data } = useQuery(GET_CURRENT_USER, { fetchPolicy: 'cache-and-network' });
+  const { data } = useQuery(GET_CURRENT_USER);
 
   return data?.getCurrentUser;
 }
