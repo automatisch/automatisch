5a177b330a175521bbef2aaf86c602e3ac725674
Ali BARIN <ali.barin53@gmail.com>
2022-08-05 13:40:03 +0200

    feat: add paging capability in getFlows query

--- packages/backend/src/graphql/queries/get-flows.ts
+++ packages/backend/src/graphql/queries/get-flows.ts
@@ -1,22 +1,31 @@
 import Context from '../../types/express/context';
+import paginate from '../../helpers/pagination';
 
 type Params = {
   appKey?: string;
+  limit: number;
+  offset: number;
 };
 
 const getFlows = async (_parent: unknown, params: Params, context: Context) => {
-  const flowsQuery = context.currentUser
-    .$relatedQuery('flows')
-    .withGraphJoined('[steps.[connection]]')
-    .orderBy('created_at', 'desc');
+  const userStepsQuery = context.currentUser
+    .$relatedQuery('steps')
+    .select('flow_id')
+    .distinctOn('flow_id');
 
   if (params.appKey) {
-    flowsQuery.where('steps.app_key', params.appKey);
+    userStepsQuery.where('app_key', params.appKey);
   }
 
-  const flows = await flowsQuery;
+  const flowsQuery = context.currentUser
+    .$relatedQuery('flows')
+    .withGraphFetched('[steps.[connection]]')
+    .whereIn(
+      'flows.id',
+      userStepsQuery
+    );
 
-  return flows;
+  return paginate(flowsQuery, params.limit, params.offset);
 };
 
 export default getFlows;
