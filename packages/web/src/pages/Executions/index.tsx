14239da162f2fb81cff54c0c95c5285379a031eb
Ali BARIN <ali.barin53@gmail.com>
2022-08-07 18:26:00 +0200

    fix: polish top whitespace in executions title

--- packages/web/src/pages/Executions/index.tsx
+++ packages/web/src/pages/Executions/index.tsx
@@ -43,7 +43,7 @@ export default function Executions(): React.ReactElement {
     <Box sx={{ py: 3 }}>
       <Container>
         <Grid container sx={{ mb: [2, 5] }} columnSpacing={1.5} rowSpacing={3}>
-          <Grid container item xs sm alignItems="center" order={{ xs: 0 }}>
+          <Grid container item xs sm alignItems="center" order={{ xs: 0, height: 80 }}>
             <PageTitle>{formatMessage('executions.title')}</PageTitle>
           </Grid>
         </Grid>
