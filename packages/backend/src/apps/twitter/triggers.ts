6537a1c64952e8a511f6e5276dd6e82e44a252e2
Faruk AYDIN <omerfaruk26@gmail.com>
2022-08-31 16:33:51 +0300

    refactor: Use plural wording for user tweets trigger

--- packages/backend/src/apps/twitter/triggers.ts
+++ packages/backend/src/apps/twitter/triggers.ts
@@ -1,15 +1,15 @@
 import TwitterClient from './client';
-import UserTweet from './triggers/user-tweet';
+import UserTweets from './triggers/user-tweets';
 import SearchTweets from './triggers/search-tweets';
 
 export default class Triggers {
   client: TwitterClient;
-  userTweet: UserTweet;
+  userTweets: UserTweets;
   searchTweets: SearchTweets;
 
   constructor(client: TwitterClient) {
     this.client = client;
-    this.userTweet = new UserTweet(client);
+    this.userTweets = new UserTweets(client);
     this.searchTweets = new SearchTweets(client);
   }
 }
