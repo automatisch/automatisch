163629b990cf9b221e6c03844fd97ead37b9c3cd
Faruk AYDIN <omerfaruk26@gmail.com>
2022-09-01 13:34:22 +0300

    feat: Implement my tweets trigger

--- packages/backend/src/apps/twitter/triggers.ts
+++ packages/backend/src/apps/twitter/triggers.ts
@@ -1,15 +1,18 @@
 import TwitterClient from './client';
 import UserTweets from './triggers/user-tweets';
 import SearchTweets from './triggers/search-tweets';
+import MyTweets from './triggers/my-tweets';
 
 export default class Triggers {
   client: TwitterClient;
   userTweets: UserTweets;
   searchTweets: SearchTweets;
+  myTweets: MyTweets;
 
   constructor(client: TwitterClient) {
     this.client = client;
     this.userTweets = new UserTweets(client);
     this.searchTweets = new SearchTweets(client);
+    this.myTweets = new MyTweets(client);
   }
 }
