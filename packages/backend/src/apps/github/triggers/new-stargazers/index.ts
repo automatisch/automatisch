e4dd7e454a54734f1c10ea648a913c1e1d962f74
Ali BARIN <ali.barin53@gmail.com>
2022-10-19 22:56:00 +0200

    feat(github): add new stargazer trigger

---
+++ packages/backend/src/apps/github/triggers/new-stargazers/index.ts
@@ -0,0 +1,46 @@
+import defineTrigger from '../../../../helpers/define-trigger';
+import newStargazers from './new-stargazers';
+
+export default defineTrigger({
+  name: 'New stargazers',
+  key: 'newStargazers',
+  pollInterval: 15,
+  description: 'Triggers when a user stars a repository',
+  substeps: [
+    {
+      key: 'chooseConnection',
+      name: 'Choose connection'
+    },
+    {
+      key: 'chooseTrigger',
+      name: 'Set up a trigger',
+      arguments: [
+        {
+          label: 'Repo',
+          key: 'repo',
+          type: 'dropdown',
+          required: true,
+          variables: false,
+          source: {
+            type: 'query',
+            name: 'getData',
+            arguments: [
+              {
+                name: 'key',
+                value: 'listRepos'
+              }
+            ]
+          }
+        },
+      ]
+    },
+    {
+      key: 'testStep',
+      name: 'Test trigger'
+    }
+  ],
+
+  async run($) {
+    return await newStargazers($);
+  },
+});
