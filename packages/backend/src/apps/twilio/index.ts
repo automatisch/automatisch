30d2740368eabcb1c9d54762dadf8c50c273a2a4
Faruk AYDIN <omerfaruk26@gmail.com>
2022-10-25 20:03:08 +0200

    feat: Implement twilio connection

---
+++ packages/backend/src/apps/twilio/index.ts
@@ -0,0 +1,13 @@
+import defineApp from '../../helpers/define-app';
+
+export default defineApp({
+  name: 'Twilio',
+  key: 'twilio',
+  iconUrl: '{BASE_URL}/apps/twilio/assets/favicon.svg',
+  authDocUrl: 'https://automatisch.io/docs/connections/twilio',
+  supportsConnections: true,
+  baseUrl: 'https://twilio.com',
+  apiBaseUrl: 'https://api.twilio.com',
+  primaryColor: 'e1000f',
+  beforeRequest: [],
+});
