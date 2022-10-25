ed1798959384754d0459a33a95ac06d6bb0b2d64
Ali BARIN <ali.barin53@gmail.com>
2022-10-25 23:25:59 +0200

    feat(flickr): add new photos in an album trigger

---
+++ packages/backend/src/apps/flickr/triggers/new-photos-in-album/index.ts
@@ -0,0 +1,47 @@
+import defineTrigger from '../../../../helpers/define-trigger';
+import newPhotosInAlbum from './new-photos-in-album';
+
+export default defineTrigger({
+  name: 'New photos in album',
+  pollInterval: 15,
+  key: 'newPhotosInAlbum',
+  description: 'Triggers when you add a new photo in an album.',
+  dedupeStrategy: 'greatest',
+  substeps: [
+    {
+      key: 'chooseConnection',
+      name: 'Choose connection'
+    },
+    {
+      key: 'chooseTrigger',
+      name: 'Set up a trigger',
+      arguments: [
+        {
+          label: 'Album',
+          key: 'album',
+          type: 'dropdown',
+          required: true,
+          variables: false,
+          source: {
+            type: 'query',
+            name: 'getData',
+            arguments: [
+              {
+                name: 'key',
+                value: 'listAlbums'
+              }
+            ]
+          }
+        }
+      ]
+    },
+    {
+      key: 'testStep',
+      name: 'Test trigger'
+    }
+  ],
+
+  async run($) {
+    await newPhotosInAlbum($);
+  },
+});
