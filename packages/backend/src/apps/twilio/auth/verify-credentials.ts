7d7c96274d2fb87b70725694b8005c9e2bc27883
Faruk AYDIN <omerfaruk26@gmail.com>
2022-10-26 18:24:12 +0200

    feat: Implement send sms action of twilio

--- packages/backend/src/apps/twilio/auth/verify-credentials.ts
+++ packages/backend/src/apps/twilio/auth/verify-credentials.ts
@@ -2,12 +2,7 @@ import { IGlobalVariable } from '@automatisch/types';
 
 const verifyCredentials = async ($: IGlobalVariable) => {
   try {
-    await $.http.get('/2010-04-01/Accounts.json?PageSize=1', {
-      auth: {
-        username: $.auth.data.accountSid as string,
-        password: $.auth.data.authToken as string,
-      },
-    });
+    await $.http.get('/2010-04-01/Accounts.json?PageSize=1');
 
     await $.auth.set({
       screenName: $.auth.data.accountSid,
