79e609e68228f26d3b0bd76e1ca7a8a91ed5ed0a
Ali BARIN <ali.barin53@gmail.com>
2022-10-30 22:28:16 +0100

    feat(discord): add list channels data

---
+++ packages/backend/src/apps/discord/data/list-channels/index.ts
@@ -0,0 +1,32 @@
+import { IGlobalVariable, IJSONObject } from '@automatisch/types';
+
+export default {
+  name: 'List channels',
+  key: 'listChannels',
+
+  async run($: IGlobalVariable) {
+    const channels: {
+      data: IJSONObject[];
+      error: IJSONObject | null;
+    } = {
+      data: [],
+      error: null,
+    };
+
+    const response = await $.http.get(`/guilds/${$.auth.data.guildId}/channels`);
+
+    channels.data = response.data
+      .filter((channel: IJSONObject) => {
+        // filter in text channels only
+        return channel.type === 0;
+      })
+      .map((channel: IJSONObject) => {
+        return {
+          value: channel.id,
+          name: channel.name,
+        };
+      });
+
+    return channels;
+  },
+};
