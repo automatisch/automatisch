dece070d28368651856041216d40233b18f92b49
Ali BARIN <ali.barin53@gmail.com>
2022-11-06 17:20:45 +0100

    feat(strava): add connection verified check

---
+++ packages/backend/src/apps/strava/auth/is-still-verified.ts
@@ -0,0 +1,13 @@
+import { IGlobalVariable } from '@automatisch/types';
+import getCurrentUser from '../common/get-current-user';
+
+const isStillVerified = async ($: IGlobalVariable) => {
+  try {
+    const user = await getCurrentUser($);
+    return !!user;
+  } catch (error) {
+    return false;
+  }
+};
+
+export default isStillVerified;
