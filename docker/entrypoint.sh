2103b4ee8cc9801d86a89c0b3c5b79b2b0566b32
Ali BARIN <ali.barin53@gmail.com>
2022-11-21 20:25:14 +0100

    fix(docker-compose): add environment variable keys

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -13,8 +13,8 @@ if [ ! -f /automatisch/storage/.env ]; then
   >&2 echo "Saving environment variables"
   ENCRYPTION_KEY="${ENCRYPTION_KEY:-$(openssl rand -base64 36)}"
   APP_SECRET_KEY="${APP_SECRET_KEY:-$(openssl rand -base64 36)}"
-  echo "$ENCRYPTION_KEY
-  $APP_SECRET_KEY" > /automatisch/storage/.env
+  echo "ENCRYPTION_KEY=$ENCRYPTION_KEY" >> /automatisch/storage/.env
+  echo "APP_SECRET_KEY=$APP_SECRET_KEY" >> /automatisch/storage/.env
 fi
 
 >&2 echo "Postgres is up - executing command"
