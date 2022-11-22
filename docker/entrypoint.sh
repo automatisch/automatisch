9ba1351a3b435c48cc74255f2d6b06cd254872cf
ksurl <ksurl@users.noreply.github.com>
2022-11-21 16:18:05 -0800

    refactor: use healthcheck for db, remove psql client from image (#737)
    * refactor: use healthcheck for db, remove postgresql client from main image
    
    * fix: redis dependency condition
    
    * cleanup entrypoint.sh
    
    * fix healthcheck command

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,13 +2,6 @@
 
 set -e
 
-export PGPASSWORD="$POSTGRES_PASSWORD"
-
-until psql -h "$POSTGRES_HOST" -U "$POSTGRES_USERNAME" -d "$POSTGRES_DATABASE" -c '\q'; do
-  >&2 echo "Waiting for Postgres to be ready..."
-  sleep 1
-done
-
 if [ ! -f /automatisch/storage/.env ]; then
   >&2 echo "Saving environment variables"
   ENCRYPTION_KEY="${ENCRYPTION_KEY:-$(openssl rand -base64 36)}"
@@ -16,8 +9,6 @@ if [ ! -f /automatisch/storage/.env ]; then
   echo "ENCRYPTION_KEY=$ENCRYPTION_KEY" >> /automatisch/storage/.env
   echo "APP_SECRET_KEY=$APP_SECRET_KEY" >> /automatisch/storage/.env
 fi
-
->&2 echo "Postgres is up - executing command"
 
 if [ -n "$WORKER" ]; then
   automatisch start-worker --env-file /automatisch/storage/.env
