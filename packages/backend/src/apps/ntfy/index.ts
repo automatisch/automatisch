46bfc597ec28999a931f858ac64bae82da8a91d6
Ali BARIN <ali.barin53@gmail.com>
2022-12-05 00:44:25 +0100

    feat(ntfy): add application

---
+++ packages/backend/src/apps/ntfy/index.ts
@@ -0,0 +1,13 @@
+import defineApp from '../../helpers/define-app';
+import addAuthHeader from './common/add-auth-header';
+
+export default defineApp({
+  name: 'Ntfy',
+  key: 'ntfy',
+  iconUrl: '{BASE_URL}/apps/ntfy/assets/favicon.svg',
+  authDocUrl: 'https://automatisch.io/docs/apps/ntfy/connection',
+  supportsConnections: true,
+  baseUrl: 'https://ntfy.sh',
+  apiBaseUrl: 'https://ntfy.sh',
+  primaryColor: '56bda8',
+});
