0fefa4e43aced070ad7605f99e7e73247f2bdba9
Faruk AYDIN <omerfaruk26@gmail.com>
2022-12-08 21:28:42 +0300

    chore: Make telegram and ntfy apps collapsed for docs sidebar

--- packages/docs/pages/.vitepress/config.js
+++ packages/docs/pages/.vitepress/config.js
@@ -72,6 +72,7 @@ export default defineConfig({
         {
           text: 'Ntfy',
           collapsible: true,
+          collapsed: true,
           items: [
             { text: 'Actions', link: '/apps/ntfy/actions' },
             { text: 'Connection', link: '/apps/ntfy/connection' },
@@ -134,6 +135,7 @@ export default defineConfig({
         {
           text: 'Telegram',
           collapsible: true,
+          collapsed: true,
           items: [
             { text: 'Actions', link: '/apps/telegram-bot/actions' },
             { text: 'Connection', link: '/apps/telegram-bot/connection' },
@@ -167,6 +169,12 @@ export default defineConfig({
             { text: 'Triggers', link: '/apps/typeform/triggers' },
             { text: 'Connection', link: '/apps/typeform/connection' },
           ],
+        },
+        {
+          text: 'Webhooks',
+          collapsible: true,
+          collapsed: true,
+          items: [{ text: 'Triggers', link: '/apps/webhooks/triggers' }],
         },
       ],
       '/': [
