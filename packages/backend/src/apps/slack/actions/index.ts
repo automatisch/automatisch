01bb1259ebfbe28bad09d1fdbb06d26e1dadb2d4
Ali BARIN <ali.barin53@gmail.com>
2023-01-30 23:37:58 +0000

    feat(slack): add find user by email action

--- packages/backend/src/apps/slack/actions/index.ts
+++ packages/backend/src/apps/slack/actions/index.ts
@@ -1,4 +1,5 @@
 import findMessage from './find-message';
+import findUserByEmail from './find-user-by-email';
 import sendMessageToChannel from './send-a-message-to-channel';
 
-export default [findMessage, sendMessageToChannel];
+export default [findMessage, findUserByEmail, sendMessageToChannel];
--- packages/docs/pages/apps/slack/actions.md
+++ packages/docs/pages/apps/slack/actions.md
@@ -3,6 +3,8 @@ favicon: /favicons/slack.svg
 items:
   - name: Find a message
     desc: Finds a message using the Slack search feature.
+  - name: Find user by email
+    desc: Finds a user by email.
   - name: Send a message to channel
     desc: Sends a message to a channel you specify.
 ---
