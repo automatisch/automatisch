07ed24ca7a88128a9b11b31f579306f16b483a33
Ali BARIN <ali.barin53@gmail.com>
2023-03-07 20:33:51 +0000

    feat: add chatwoot

--- packages/web/src/config/app.ts
+++ packages/web/src/config/app.ts
@@ -6,6 +6,7 @@ const config: Config = {
   baseUrl: process.env.REACT_APP_BASE_URL as string,
   graphqlUrl: process.env.REACT_APP_GRAPHQL_URL as string,
   notificationsUrl: process.env.REACT_APP_NOTIFICATIONS_URL as string,
+  chatwootBaseUrl: 'https://app.chatwoot.com',
 };
 
 export default config;
