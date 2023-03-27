fd24dbee218234c4e1a9805d37aa72cec9fc9029
Ali BARIN <ali.barin53@gmail.com>
2023-03-27 08:22:14 +0000

    fix(useBillingAndUsageData): fix invalid date

--- packages/web/src/hooks/useBillingAndUsageData.ee.ts
+++ packages/web/src/hooks/useBillingAndUsageData.ee.ts
@@ -7,7 +7,8 @@ import { GET_BILLING_AND_USAGE } from 'graphql/queries/get-billing-and-usage.ee'
 function transform(billingAndUsageData: NonNullable<UseBillingAndUsageDataReturn>) {
   const nextBillDate = billingAndUsageData.subscription.nextBillDate;
   const nextBillDateTitle = nextBillDate.title;
-  const relativeNextBillDateTitle = nextBillDateTitle ? DateTime.fromMillis(Number(nextBillDateTitle)).toFormat('LLL dd, yyyy') as string : '';
+  const nextBillDateTitleDateObject = DateTime.fromMillis(Number(nextBillDateTitle));
+  const formattedNextBillDateTitle = nextBillDateTitleDateObject.isValid ? nextBillDateTitleDateObject.toFormat('LLL dd, yyyy') : nextBillDateTitle;
 
   return {
     ...billingAndUsageData,
@@ -15,7 +16,7 @@ function transform(billingAndUsageData: NonNullable<UseBillingAndUsageDataReturn
       ...billingAndUsageData.subscription,
       nextBillDate: {
         ...billingAndUsageData.subscription.nextBillDate,
-        title: relativeNextBillDateTitle,
+        title: formattedNextBillDateTitle,
       }
     }
   };
