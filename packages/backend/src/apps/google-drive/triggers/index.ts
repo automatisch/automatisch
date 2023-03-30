4931cbcc34939d2d321de295c5655fcd488acfc6
Rıdvan Akca <akca.ridvann@gmail.com>
2023-03-30 15:37:57 +0300

    feat(google-drive): add updated files trigger

--- packages/backend/src/apps/google-drive/triggers/index.ts
+++ packages/backend/src/apps/google-drive/triggers/index.ts
@@ -1,5 +1,6 @@
 import newFiles from './new-files';
 import newFilesInFolder from './new-files-in-folder';
 import newFolders from './new-folders';
+import updatedFiles from './updated-files';
 
-export default [newFiles, newFilesInFolder, newFolders];
+export default [newFiles, newFilesInFolder, newFolders, updatedFiles];
---
+++ packages/backend/src/apps/google-drive/triggers/updated-files/updated-files.ts
@@ -0,0 +1,40 @@
+import { IGlobalVariable } from '@automatisch/types';
+
+const updatedFiles = async ($: IGlobalVariable) => {
+  let q = `mimeType!='application/vnd.google-apps.folder'`;
+  if ($.step.parameters.includeDeleted === false) {
+    q += ` and trashed=${$.step.parameters.includeDeleted}`;
+  }
+
+  if ($.step.parameters.folderId) {
+    q += ` and '${$.step.parameters.folderId}' in parents`;
+  } else {
+    q += ` and parents in 'root'`;
+  }
+
+  const params = {
+    pageToken: undefined as unknown as string,
+    orderBy: 'modifiedTime desc',
+    fields: '*',
+    pageSize: 1000,
+    q,
+  };
+
+  do {
+    const { data } = await $.http.get(`/v3/files`, { params });
+    params.pageToken = data.nextPageToken;
+
+    if (data.files?.length) {
+      for (const file of data.files) {
+        $.pushTriggerItem({
+          raw: file,
+          meta: {
+            internalId: `${file.id}-${file.modifiedTime}`,
+          },
+        });
+      }
+    }
+  } while (params.pageToken);
+};
+
+export default updatedFiles;
