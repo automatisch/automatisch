c12703422cf51eb2c96ca1c0701303edaef3a779
Faruk AYDIN <omerfaruk26@gmail.com>
2023-04-26 12:13:39 +0200

    feat: Introduce new plans for the cloud

--- packages/backend/src/helpers/billing/plans.ee.ts
+++ packages/backend/src/helpers/billing/plans.ee.ts
@@ -1,12 +1,26 @@
-const plans = [
+import appConfig from '../../config/app';
+
+const testPlans = [
   {
     name: '10k - monthly',
     limit: '10,000',
     quota: 10000,
     price: '€20',
     productId: '47384',
-  }
+  },
 ];
+
+const prodPlans = [
+  {
+    name: '10k - monthly',
+    limit: '10,000',
+    quota: 10000,
+    price: '€20',
+    productId: '826658',
+  },
+];
+
+const plans = appConfig.isProd ? prodPlans : testPlans;
 
 export function getPlanById(id: string) {
   return plans.find((plan) => plan.productId === id);
