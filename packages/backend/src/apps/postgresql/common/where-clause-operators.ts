877bd24836e59dbba3e4b6a647131bf46837ffc7
Ali BARIN <ali.barin53@gmail.com>
2023-05-09 21:08:32 +0000

    feat(postgresql/update): add interactive where clause

---
+++ packages/backend/src/apps/postgresql/common/where-clause-operators.ts
@@ -0,0 +1,60 @@
+const whereClauseOperators = [
+  {
+    value: "=",
+    label: "="
+  },
+  {
+    value: ">",
+    label: ">"
+  },
+  {
+    value: "<",
+    label: "<"
+  },
+  {
+    value: ">=",
+    label: ">="
+  },
+  {
+    value: "<=",
+    label: "<="
+  },
+  {
+    value: "<>",
+    label: "<>"
+  },
+  {
+    value: "!=",
+    label: "!="
+  },
+  {
+    value: "AND",
+    label: "AND"
+  },
+  {
+    value: "OR",
+    label: "OR"
+  },
+  {
+    value: "IN",
+    label: "IN"
+  },
+  {
+    value: "BETWEEN",
+    label: "BETWEEN"
+  },
+  {
+    value: "LIKE",
+    label: "LIKE"
+  },
+  {
+    value: "IS NULL",
+    label: "IS NULL"
+  },
+  {
+    value: "NOT",
+    label: "NOT"
+  }
+];
+
+export default whereClauseOperators;
