75d5c0e356664f378a7c5bedbe9f753e71f70ddb
Ali BARIN <ali.barin53@gmail.com>
2023-06-04 21:18:22 +0000

    feat: prevent from being used in iframe

--- packages/backend/src/helpers/web-ui-handler.ts
+++ packages/backend/src/helpers/web-ui-handler.ts
@@ -10,7 +10,12 @@ const webUIHandler = async (app: Application) => {
   const indexHtml = join(dirname(webAppPath), 'build', 'index.html');
 
   app.use(express.static(webBuildPath));
-  app.get('*', (_req, res) => res.sendFile(indexHtml));
+  app.get('*', (_req, res) => {
+    res.set('Content-Security-Policy', 'frame-ancestors: none;');
+    res.set('X-Frame-Options', 'DENY');
+
+    res.sendFile(indexHtml);
+  });
 };
 
 export default webUIHandler;
