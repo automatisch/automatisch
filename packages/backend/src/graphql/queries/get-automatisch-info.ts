84b701747fb61f92f8221e2c7b26f7b151d2212f
Ömer Faruk Aydın <omerfaruk26@gmail.com>
2023-08-10 23:18:10 +0200

    feat: add license info in getAutomatischInfo query (#1202)

--- packages/backend/src/graphql/queries/get-automatisch-info.ts
+++ packages/backend/src/graphql/queries/get-automatisch-info.ts
@@ -1,8 +1,19 @@
 import appConfig from '../../config/app';
+import { getLicense } from '../../helpers/license.ee';
 
 const getAutomatischInfo = async () => {
+  const license = await getLicense();
+
+  const computedLicense = {
+    id: license ? license.id : null,
+    name: license ? license.name : null,
+    expireAt: license ? license.expireAt : null,
+    verified: license ? true : false,
+  };
+
   return {
     isCloud: appConfig.isCloud,
+    license: computedLicense,
   };
 };
 
