03ce088ce94a08d8b2fc556c5f40449faf05e2a1
Rıdvan Akca <akca.ridvann@gmail.com>
2023-08-10 16:00:49 +0300

    test: rewrite connections tests with playwright

--- packages/e2e-tests/fixtures/index.js
+++ packages/e2e-tests/fixtures/index.js
@@ -1,9 +1,13 @@
 const base = require('@playwright/test');
 const { ApplicationsPage } = require('./applications-page');
+const { ConnectionsPage } = require('./connections-page');
 
 exports.test = base.test.extend({
   applicationsPage: async ({ page }, use) => {
     await use(new ApplicationsPage(page));
+  },
+  connectionsPage: async ({ page }, use) => {
+    await use(new ConnectionsPage(page));
   },
 });
 exports.expect = base.expect;
