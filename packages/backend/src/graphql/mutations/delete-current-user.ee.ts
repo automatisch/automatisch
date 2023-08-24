4b77f2f5902eb6513398c6f0fe54e28cb98075f0
Faruk AYDIN <omerfaruk26@gmail.com>
2023-08-24 12:48:37 +0200

    fix: Remove deleted flows from Redis

--- packages/backend/src/graphql/mutations/delete-current-user.ee.ts
+++ packages/backend/src/graphql/mutations/delete-current-user.ee.ts
@@ -1,17 +1,44 @@
 import { Duration } from 'luxon';
 import Context from '../../types/express/context';
 import deleteUserQueue from '../../queues/delete-user.ee';
+import flowQueue from '../../queues/flow';
+import Flow from '../../models/flow';
 
-const deleteCurrentUser = async (_parent: unknown, params: never, context: Context) => {
+const deleteCurrentUser = async (
+  _parent: unknown,
+  params: never,
+  context: Context
+) => {
   const id = context.currentUser.id;
 
+  const flows = await context.currentUser.$relatedQuery('flows').where({
+    status: 'active',
+  });
+
+  const repeatableJobs = await flowQueue.getRepeatableJobs();
+
+  for (const flow of flows) {
+    const job = repeatableJobs.find((job) => job.id === flow.id);
+
+    if (job) {
+      await flowQueue.removeRepeatableByKey(job.key);
+    }
+  }
+
   await context.currentUser.$query().delete();
+
+  await Flow.query()
+    .whereIn(
+      'id',
+      flows.map((flow) => flow.id)
+    )
+    .delete();
 
   const jobName = `Delete user - ${id}`;
   const jobPayload = { id };
   const millisecondsFor30Days = Duration.fromObject({ days: 30 }).toMillis();
   const jobOptions = {
-    delay: millisecondsFor30Days
+    delay: millisecondsFor30Days,
   };
 
   await deleteUserQueue.add(jobName, jobPayload, jobOptions);
