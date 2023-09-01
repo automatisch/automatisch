8e256fe38e3632451f96f16b9ec0b8eec624a06f
Faruk AYDIN <omerfaruk26@gmail.com>
2023-09-01 18:01:02 +0200

    chore: Sort formatter text transformers alphabetically

--- packages/backend/src/apps/formatter/actions/text/index.ts
+++ packages/backend/src/apps/formatter/actions/text/index.ts
@@ -1,26 +1,26 @@
-import defineAction from '../../../../helpers/define-action';
 import capitalize from './transformers/capitalize';
-import htmlToMarkdown from './transformers/html-to-markdown';
-import markdownToHtml from './transformers/markdown-to-html';
-import useDefaultValue from './transformers/use-default-value';
+import defineAction from '../../../../helpers/define-action';
 import extractEmailAddress from './transformers/extract-email-address';
 import extractNumber from './transformers/extract-number';
+import htmlToMarkdown from './transformers/html-to-markdown';
 import lowercase from './transformers/lowercase';
+import markdownToHtml from './transformers/markdown-to-html';
 import pluralize from './transformers/pluralize';
-import trimWhitespace from './transformers/trim-whitespace';
 import replace from './transformers/replace';
+import trimWhitespace from './transformers/trim-whitespace';
+import useDefaultValue from './transformers/use-default-value';
 
 const transformers = {
   capitalize,
-  htmlToMarkdown,
-  markdownToHtml,
-  useDefaultValue,
   extractEmailAddress,
   extractNumber,
+  htmlToMarkdown,
   lowercase,
+  markdownToHtml,
   pluralize,
-  trimWhitespace,
   replace,
+  trimWhitespace,
+  useDefaultValue,
 };
 
 export default defineAction({
@@ -40,13 +40,13 @@ export default defineAction({
         { label: 'Capitalize', value: 'capitalize' },
         { label: 'Convert HTML to Markdown', value: 'htmlToMarkdown' },
         { label: 'Convert Markdown to HTML', value: 'markdownToHtml' },
-        { label: 'Use Default Value', value: 'useDefaultValue' },
         { label: 'Extract Email Address', value: 'extractEmailAddress' },
         { label: 'Extract Number', value: 'extractNumber' },
         { label: 'Lowercase', value: 'lowercase' },
         { label: 'Pluralize', value: 'pluralize' },
-        { label: 'Trim Whitespace', value: 'trimWhitespace' },
         { label: 'Replace', value: 'replace' },
+        { label: 'Trim Whitespace', value: 'trimWhitespace' },
+        { label: 'Use Default Value', value: 'useDefaultValue' },
       ],
       additionalFields: {
         type: 'query',
--- packages/backend/src/apps/formatter/dynamic-fields/list-transform-options/index.ts
+++ packages/backend/src/apps/formatter/dynamic-fields/list-transform-options/index.ts
@@ -1,26 +1,26 @@
 import { IGlobalVariable, IJSONObject } from '@automatisch/types';
 import capitalize from './options/capitalize';
-import htmlToMarkdown from './options/html-to-markdown';
-import markdownToHtml from './options/markdown-to-html';
-import useDefaultValue from './options/use-default-value';
 import extractEmailAddress from './options/extract-email-address';
 import extractNumber from './options/extract-number';
+import htmlToMarkdown from './options/html-to-markdown';
 import lowercase from './options/lowercase';
+import markdownToHtml from './options/markdown-to-html';
 import pluralize from './options/pluralize';
-import trimWhitespace from './options/trim-whitespace';
 import replace from './options/replace';
+import trimWhitespace from './options/trim-whitespace';
+import useDefaultValue from './options/use-default-value';
 
 const options: IJSONObject = {
   capitalize,
-  htmlToMarkdown,
-  markdownToHtml,
-  useDefaultValue,
   extractEmailAddress,
   extractNumber,
+  htmlToMarkdown,
   lowercase,
+  markdownToHtml,
   pluralize,
-  trimWhitespace,
   replace,
+  trimWhitespace,
+  useDefaultValue,
 };
 
 export default {
