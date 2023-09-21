f53909355fa2941d53cb65bd465fa7ecf63b2867
Rıdvan Akca <akca.ridvann@gmail.com>
2023-09-21 16:25:36 +0300

    feat(pipedrive): add pipedrive integration

---
+++ packages/backend/src/apps/pipedrive/auth/verify-credentials.ts
@@ -0,0 +1,55 @@
+import { IField, IGlobalVariable } from '@automatisch/types';
+import { URLSearchParams } from 'url';
+import getCurrentUser from '../common/get-current-user';
+
+const verifyCredentials = async ($: IGlobalVariable) => {
+  const oauthRedirectUrlField = $.app.auth.fields.find(
+    (field: IField) => field.key == 'oAuthRedirectUrl'
+  );
+  const redirectUri = oauthRedirectUrlField.value as string;
+  const params = new URLSearchParams({
+    grant_type: 'authorization_code',
+    code: $.auth.data.code as string,
+    redirect_uri: redirectUri,
+  });
+
+  const headers = {
+    Authorization: `Basic ${Buffer.from(
+      $.auth.data.clientId + ':' + $.auth.data.clientSecret
+    ).toString('base64')}`,
+    'Content-Type': 'application/x-www-form-urlencoded',
+  };
+
+  const response = await $.http.post(
+    `https://oauth.pipedrive.com/oauth/token`,
+    params.toString(),
+    { headers }
+  );
+
+  const {
+    access_token: accessToken,
+    api_domain: apiDomain,
+    expires_in: expiresIn,
+    refresh_token: refreshToken,
+    scope: scope,
+    token_type: tokenType,
+  } = response.data;
+
+  await $.auth.set({
+    accessToken,
+    apiDomain,
+    expiresIn,
+    refreshToken,
+    scope,
+    tokenType,
+  });
+
+  const user = await getCurrentUser($);
+
+  await $.auth.set({
+    userId: user.id,
+    screenName: user.name,
+  });
+};
+
+export default verifyCredentials;
---
+++ packages/backend/src/apps/pipedrive/index.ts
@@ -0,0 +1,16 @@
+import defineApp from '../../helpers/define-app';
+import addAuthHeader from './common/add-auth-header';
+import auth from './auth';
+
+export default defineApp({
+  name: 'Pipedrive',
+  key: 'pipedrive',
+  baseUrl: '',
+  apiBaseUrl: '',
+  iconUrl: '{BASE_URL}/apps/pipedrive/assets/favicon.svg',
+  authDocUrl: 'https://automatisch.io/docs/apps/pipedrive/connection',
+  primaryColor: 'FFFFFF',
+  supportsConnections: true,
+  beforeRequest: [addAuthHeader],
+  auth,
+});
