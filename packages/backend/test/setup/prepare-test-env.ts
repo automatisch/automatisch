4ab6415f49a0e9067fd62483747f279c603f8a7b
Faruk AYDIN <omerfaruk26@gmail.com>
2023-10-01 17:55:30 +0200

    feat: Migrate test database within pretest script

--- packages/backend/test/setup/prepare-test-env.ts
+++ packages/backend/test/setup/prepare-test-env.ts
@@ -1,2 +1,22 @@
 import './check-env-file';
-import './create-database';
+import { createDatabaseAndUser } from '../../bin/database/utils';
+import { client as knex } from '../../src/config/database';
+import logger from '../../src/helpers/logger';
+
+const createAndMigrateDatabase = async () => {
+  await createDatabaseAndUser();
+  const migrator = knex.migrate;
+
+  await migrator.latest();
+
+  logger.info(`Completed database migrations for the test database.`);
+};
+
+createAndMigrateDatabase()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((error) => {
+    logger.error(error);
+    process.exit(1);
+  });
