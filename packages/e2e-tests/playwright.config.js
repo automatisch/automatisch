24fb30519b91b1b25aba3f8f0e211ebfcafa231a
QAComet <lucas@qacomet.com>
2023-10-05 20:42:09 -0600

    chore: testing out multiple reporters with html

--- packages/e2e-tests/playwright.config.js
+++ packages/e2e-tests/playwright.config.js
@@ -22,7 +22,14 @@ module.exports = defineConfig({
   /* Timeout threshold for each test */
   timeout: 30000,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? 'github' : 'html',
+  reporter: process.env.CI
+    ? [
+        ['html', { open: 'never' }],
+        ['github']
+      ]
+    : [
+      ['html', { open: 'never' }],
+    ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
