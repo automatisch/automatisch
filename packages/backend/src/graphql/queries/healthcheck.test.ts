eaa25e412a1655f8ddd7da9267155c388a2c6cf9
Faruk AYDIN <omerfaruk26@gmail.com>
2023-10-14 17:19:44 +0200

    test: Add test for healthcheck graphQL query

---
+++ packages/backend/src/graphql/queries/healthcheck.test.ts
@@ -0,0 +1,26 @@
+import request from 'supertest';
+import app from '../../app';
+import appConfig from '../../config/app';
+
+describe('graphQL healthcheck query', () => {
+  it('should return application version', async () => {
+    const query = `
+      query {
+        healthcheck {
+          version
+        }
+      }
+    `;
+
+    const response = await request(app)
+      .post('/graphql')
+      .send({ query })
+      .expect(200);
+
+    const expectedResponsePayload = {
+      data: { healthcheck: { version: appConfig.version } },
+    };
+
+    expect(response.body).toEqual(expectedResponsePayload);
+  });
+});
