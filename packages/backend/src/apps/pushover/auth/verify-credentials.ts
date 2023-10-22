c4394228f28f355cda7b13fe2e378fb2dcce858f
Faruk AYDIN <omerfaruk26@gmail.com>
2023-10-22 23:03:16 +0200

    fix(placetel): Authenticate in case valid user without device

--- packages/backend/src/apps/pushover/auth/verify-credentials.ts
+++ packages/backend/src/apps/pushover/auth/verify-credentials.ts
@@ -1,11 +1,21 @@
 import { IGlobalVariable } from '@automatisch/types';
+import HttpError from '../../../errors/http';
 
 const verifyCredentials = async ($: IGlobalVariable) => {
-  await $.http.post(`/1/messages.json`, {
-    token: $.auth.data.apiToken as string,
-    user: $.auth.data.userKey as string,
-    message: 'hello world',
-  });
+  try {
+    await $.http.post(`/1/users/validate.json`, {
+      token: $.auth.data.apiToken as string,
+      user: $.auth.data.userKey as string,
+    });
+  } catch (error) {
+    const noDeviceError = 'user is valid but has no active devices';
+    const hasNoDeviceError =
+      error.response?.data?.errors?.includes(noDeviceError);
+
+    if (!hasNoDeviceError) {
+      throw new HttpError(error);
+    }
+  }
 
   await $.auth.set({
     screenName: $.auth.data.screenName,
