945c52dd6b4487c33af2a75763b47123c6d5b4ab
Faruk AYDIN <omerfaruk26@gmail.com>
2023-10-26 17:16:42 +0200

    test: Add createdAt and updatedAt defaultst to execution and flow

--- packages/backend/test/factories/execution.ts
+++ packages/backend/test/factories/execution.ts
@@ -4,6 +4,8 @@ import { createFlow } from './flow';
 export const createExecution = async (params: Partial<Execution> = {}) => {
   params.flowId = params?.flowId || (await createFlow()).id;
   params.testRun = params?.testRun || false;
+  params.createdAt = params?.createdAt || new Date().toISOString();
+  params.updatedAt = params?.updatedAt || new Date().toISOString();
 
   const [execution] = await global.knex
     .table('executions')
