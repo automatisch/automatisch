4af761a9e366c5a5b7797a1b14ad6a09152fa188
Rıdvan Akca <akca.ridvann@gmail.com>
2023-12-07 16:28:27 +0300

    feat(better-stack): add better stack integration

---
+++ packages/backend/src/apps/better-stack/auth/index.ts
@@ -0,0 +1,33 @@
+import verifyCredentials from './verify-credentials';
+import isStillVerified from './is-still-verified';
+
+export default {
+  fields: [
+    {
+      key: 'screenName',
+      label: 'Screen Name',
+      type: 'string' as const,
+      required: true,
+      readOnly: false,
+      value: null,
+      placeholder: null,
+      description:
+        'Screen name of your connection to be used on Automatisch UI.',
+      clickToCopy: false,
+    },
+    {
+      key: 'apiKey',
+      label: 'API Key',
+      type: 'string' as const,
+      required: true,
+      readOnly: false,
+      value: null,
+      placeholder: null,
+      description: 'Better Stack API key of your account.',
+      clickToCopy: false,
+    },
+  ],
+
+  verifyCredentials,
+  isStillVerified,
+};
---
+++ packages/docs/pages/apps/better-stack/connection.md
@@ -0,0 +1,14 @@
+# Better Stack
+
+:::info
+This page explains the steps you need to follow to set up the Better Stack
+connection in Automatisch. If any of the steps are outdated, please let us know!
+:::
+
+1. Login to your Better Stack account: [https://betterstack.com/](https://betterstack.com/).
+2. Click on the team name bottom left and select **Manage Teams** option.
+3. Click on the three dots icon of your team and select **manage** option.
+4. Click on the **API tokens** tab.
+5. Copy the token next to **Direct API tokens** to the `API Key` field on Automatisch.
+6. Fill the screen name on Automatisch.
+7. Now, you can start using the Better Stack connection with Automatisch.
