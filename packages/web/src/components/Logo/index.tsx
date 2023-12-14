8608431490c28410226dd6b7c406c6fac5e5ca56
Ali BARIN <ali.barin53@gmail.com>
2023-12-14 15:58:40 +0000

    feat: add conditional mation logo by default

--- packages/web/src/components/Logo/index.tsx
+++ packages/web/src/components/Logo/index.tsx
@@ -1,8 +1,7 @@
-import Typography from '@mui/material/Typography';
 import * as React from 'react';
-import { FormattedMessage } from 'react-intl';
 
 import CustomLogo from 'components/CustomLogo/index.ee';
+import DefaultLogo from 'components/DefaultLogo';
 import useConfig from 'hooks/useConfig';
 
 const Logo = () => {
@@ -13,11 +12,7 @@ const Logo = () => {
 
   if (logoSvgData) return <CustomLogo />;
 
-  return (
-    <Typography variant="h6" component="h1" data-test="typography-logo" noWrap>
-      <FormattedMessage id="brandText" />
-    </Typography>
-  );
+  return <DefaultLogo />;
 };
 
 export default Logo;
---
+++ packages/web/src/components/MationLogo/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { ReactComponent as MationLogoSvg } from './assets/mation-logo.svg';
+
+const MationLogo = () => {
+  return <MationLogoSvg />;
+};
+
+export default MationLogo;
