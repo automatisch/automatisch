c4a6c86a694c72e2e8a7ae69a3bc1e463829a1f0
Faruk AYDIN <omerfaruk26@gmail.com>
2024-01-08 18:36:36 +0100

    feat: Make web app path static for the backend

--- packages/backend/src/helpers/web-ui-handler.js
+++ packages/backend/src/helpers/web-ui-handler.js
@@ -1,19 +1,19 @@
 import express from 'express';
-import { dirname, join } from 'path';
+import path, { join } from 'path';
 import appConfig from '../config/app.js';
 import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const webUIHandler = async (app) => {
   if (appConfig.serveWebAppSeparately) return;
 
-  const moduleURL = new URL('@automatisch/web/package.json', import.meta.url);
-  const modulePath = fileURLToPath(moduleURL);
-  const webAppPath = dirname(modulePath);
-
-  const webBuildPath = join(dirname(webAppPath), 'build');
-  const indexHtml = join(dirname(webAppPath), 'build', 'index.html');
+  const webAppPath = join(__dirname, '../../../web/');
+  const webBuildPath = join(webAppPath, 'build');
+  const indexHtml = join(webAppPath, 'build', 'index.html');
 
   app.use(express.static(webBuildPath));
+
   app.get('*', (_req, res) => {
     res.set('Content-Security-Policy', 'frame-ancestors: none;');
     res.set('X-Frame-Options', 'DENY');
