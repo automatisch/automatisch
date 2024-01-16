159f017a44867a71a1e982288ac15a88d54db079
Rıdvan Akca <akca.ridvann@gmail.com>
2024-01-16 13:08:16 +0300

    feat(better-stack): add resolve incident action

--- packages/backend/src/apps/better-stack/actions/index.js
+++ packages/backend/src/apps/better-stack/actions/index.js
@@ -1,4 +1,5 @@
 import acknowledgeIncident from './acknowledge-incident/index.js';
 import createIncident from './create-incident/index.js';
+import resolveIncident from './resolve-incident/index.js';
 
-export default [acknowledgeIncident, createIncident];
+export default [acknowledgeIncident, createIncident, resolveIncident];
