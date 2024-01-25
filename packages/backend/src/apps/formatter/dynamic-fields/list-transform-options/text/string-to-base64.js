ef34068ac4898bc6fee58c94656f6484e0ea11ed
Rıdvan Akca <akca.ridvann@gmail.com>
2024-01-25 18:50:41 +0300

    feat(formatter): add string to base64 action

---
+++ packages/backend/src/apps/formatter/dynamic-fields/list-transform-options/text/string-to-base64.js
@@ -0,0 +1,12 @@
+const stringToBase64 = [
+  {
+    label: 'Input',
+    key: 'input',
+    type: 'string',
+    required: true,
+    description: 'Text that will be converted to Base64.',
+    variables: true,
+  },
+];
+
+export default stringToBase64;
