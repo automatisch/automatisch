f8bd48814c909bc6fec1558896fedd1b720b7975
Chris Qiang <chris@christianto.net>
2024-02-08 20:50:16 +0700

    feat(gitlab): Add single issue getter

---
+++ packages/backend/src/apps/gitlab/actions/issues/get.js
@@ -0,0 +1,36 @@
+import defineAction from '../../../../helpers/define-action.js';
+
+export default defineAction({
+  name: 'Get Single Project Issue',
+  key: 'singleProjectIssueGet',
+  description: 'Get a single project issue.',
+  arguments: [
+    {
+      label: 'Project ID',
+      key: 'id',
+      type: 'string',
+      required: true,
+      description:
+        'The global ID or URL-encoded path of the project owned by the authenticated user.',
+      variables: true,
+    },
+    {
+      label: 'Issue IID',
+      key: 'issue_iid',
+      type: 'integer',
+      required: true,
+      description: "The internal ID of a project's issue.",
+      variables: true,
+    },
+  ],
+
+  async run($) {
+    const { id, issue_iid } = $.step.parameters;
+
+    const response = await $.http.get(
+      `/api/v4/projects/${encodeURI(id)}/issues/${issue_iid}`
+    );
+
+    $.setActionItem({ raw: response.data });
+  },
+});
