da732becb63c3d81efd61f50f3b23e46a051c945
Faruk AYDIN <omerfaruk26@gmail.com>
2024-02-14 00:52:17 +0100

    feat: Implement api/v1/users API endpoint

--- packages/backend/src/routes/api/v1/users.js
+++ packages/backend/src/routes/api/v1/users.js
@@ -3,9 +3,11 @@ import { authenticateUser } from '../../../helpers/authentication.js';
 import { authorizeUser } from '../../../helpers/authorization.js';
 import getCurrentUserAction from '../../../controllers/api/v1/users/get-current-user.js';
 import getUserAction from '../../../controllers/api/v1/users/get-user.js';
+import getUsersAction from '../../../controllers/api/v1/users/get-users.js';
 
 const router = Router();
 
+router.get('/', authenticateUser, authorizeUser, getUsersAction);
 router.get('/me', authenticateUser, getCurrentUserAction);
 router.get('/:userId', authenticateUser, authorizeUser, getUserAction);
 
