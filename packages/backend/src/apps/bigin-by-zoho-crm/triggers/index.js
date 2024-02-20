43c34fcb7b4f4119928481ce43067ebbeb0cf085
Rıdvan Akca <akca.ridvann@gmail.com>
2024-02-20 17:31:30 +0300

    feat(bigin-by-zoho-crm): add new products trigger

--- packages/backend/src/apps/bigin-by-zoho-crm/triggers/index.js
+++ packages/backend/src/apps/bigin-by-zoho-crm/triggers/index.js
@@ -1,6 +1,7 @@
 import newCalls from './new-calls/index.js';
 import newCompanies from './new-companies/index.js';
 import newContacts from './new-contacts/index.js';
+import newProducts from './new-products/index.js';
 import newTasks from './new-tasks/index.js';
 
-export default [newCalls, newCompanies, newContacts, newTasks];
+export default [newCalls, newCompanies, newContacts, newProducts, newTasks];
