b21074c871d4522bdc6512a5b903526fecb9a546
Faruk AYDIN <omerfaruk26@gmail.com>
2024-02-21 14:59:42 +0100

    fix: Move saml auth provider router to correct folder

--- packages/backend/src/routes/index.js
+++ packages/backend/src/routes/index.js
@@ -5,7 +5,7 @@ import paddleRouter from './paddle.ee.js';
 import healthcheckRouter from './healthcheck.js';
 import automatischRouter from './api/v1/automatisch.js';
 import usersRouter from './api/v1/users.js';
-import samlAuthProvidersRouter from './api/v1/saml-auth-providers.ee.js';
+import samlAuthProvidersRouter from './api/v1/admin/saml-auth-providers.ee.js';
 
 const router = Router();
 
