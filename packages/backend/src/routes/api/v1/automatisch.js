de0bd2f48697f2c6b3548ef4c33e7ce1c237c4a5
Faruk AYDIN <omerfaruk26@gmail.com>
2024-02-25 03:22:03 +0100

    faet: Implement automatisch license API endpoint

--- packages/backend/src/routes/api/v1/automatisch.js
+++ packages/backend/src/routes/api/v1/automatisch.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import versionAction from '../../../controllers/api/v1/automatisch/version.js';
 import notificationsAction from '../../../controllers/api/v1/automatisch/notifications.js';
 import infoAction from '../../../controllers/api/v1/automatisch/info.js';
+import licenseAction from '../../../controllers/api/v1/automatisch/license.js';
 
 const router = Router();
 
 router.get('/version', versionAction);
 router.get('/notifications', notificationsAction);
 router.get('/info', infoAction);
+router.get('/license', licenseAction);
 
 export default router;
---
+++ packages/backend/test/mocks/rest/api/v1/automatisch/license.js
@@ -0,0 +1,19 @@
+const licenseMock = () => {
+  return {
+    data: {
+      expireAt: '2025-12-31T23:59:59Z',
+      id: '123',
+      name: 'license-name',
+      verified: true,
+    },
+    meta: {
+      count: 1,
+      currentPage: null,
+      isArray: false,
+      totalPages: null,
+      type: 'Object',
+    },
+  };
+};
+
+export default licenseMock;
