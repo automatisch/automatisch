079fb5d108f36b2e3d5a67f28d0b92eb62bc06da
Faruk AYDIN <omerfaruk26@gmail.com>
2024-02-25 03:15:55 +0100

    feat: Implement automatisch info API endpoint

--- packages/backend/src/routes/api/v1/automatisch.js
+++ packages/backend/src/routes/api/v1/automatisch.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import versionAction from '../../../controllers/api/v1/automatisch/version.js';
 import notificationsAction from '../../../controllers/api/v1/automatisch/notifications.js';
+import infoAction from '../../../controllers/api/v1/automatisch/info.js';
 
 const router = Router();
 
 router.get('/version', versionAction);
 router.get('/notifications', notificationsAction);
+router.get('/info', infoAction);
 
 export default router;
---
+++ packages/backend/test/mocks/rest/api/v1/automatisch/info.js
@@ -0,0 +1,18 @@
+const infoMock = () => {
+  return {
+    data: {
+      isCloud: false,
+      isMation: false,
+      isEnterprise: true,
+    },
+    meta: {
+      count: 1,
+      currentPage: null,
+      isArray: false,
+      totalPages: null,
+      type: 'Object',
+    },
+  };
+};
+
+export default infoMock;
