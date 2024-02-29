83815d3caae51b0cf98d2ea2a65887b9bfe4946e
Ali BARIN <ali.barin53@gmail.com>
2024-02-29 17:34:18 +0000

    feat: introduce react-query and react-query-devtools

---
+++ packages/web/src/components/QueryClientProvider/index.jsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import api from 'helpers/api.js';
+import useAuthentication from 'hooks/useAuthentication.js';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      // provides a convenient default while it should be overridden for other HTTP methods
+      queryFn: async ({ queryKey, signal }) => {
+        const { data } = await api.get(queryKey[0], {
+          signal,
+        });
+
+        return data;
+      },
+      retry: false,
+    },
+  },
+});
+
+export default function AutomatischQueryClientProvider({ children }) {
+  const { token, initialize } = useAuthentication();
+
+  React.useEffect(
+    function updateTokenInHttpClient() {
+      if (!initialize) return;
+
+      if (token) {
+        api.defaults.headers.Authorization = token;
+
+        initialize();
+      } else {
+        delete api.defaults.headers.Authorization;
+      }
+    },
+    [initialize, token],
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
+
+AutomatischQueryClientProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
--- packages/web/src/config/app.js
+++ packages/web/src/config/app.js
@@ -1,6 +1,7 @@
 const config = {
   baseUrl: process.env.REACT_APP_BASE_URL,
   graphqlUrl: process.env.REACT_APP_GRAPHQL_URL,
+  restApiUrl: process.env.REACT_APP_REST_API_URL,
   chatwootBaseUrl: 'https://app.chatwoot.com',
   supportEmailAddress: 'support@automatisch.io',
 };
