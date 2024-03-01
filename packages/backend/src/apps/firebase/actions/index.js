1983f636283503ddf8f2b68296ee6af9f568cb51
Rıdvan Akca <akca.ridvann@gmail.com>
2024-03-01 15:36:32 +0300

    feat(firebase): add find cloud firestore document action

--- packages/backend/src/apps/firebase/actions/index.js
+++ packages/backend/src/apps/firebase/actions/index.js
@@ -1,3 +1,4 @@
 import createCloudFirestoreDocument from './create-cloud-firestore-document/index.js';
+import findCloudFirestoreDocument from './find-cloud-firestore-document/index.js';
 
-export default [createCloudFirestoreDocument];
+export default [createCloudFirestoreDocument, findCloudFirestoreDocument];
--- packages/docs/pages/apps/firebase/actions.md
+++ packages/docs/pages/apps/firebase/actions.md
@@ -3,6 +3,8 @@ favicon: /favicons/firebase.svg
 items:
   - name: Create Cloud Firestore document
     desc: Creates a new document within a Cloud Firestore collection.
+  - name: Find Cloud Firestore document
+    desc: Finds a document within a collection.
 ---
 
 <script setup>
