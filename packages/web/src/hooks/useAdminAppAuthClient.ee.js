6a58d1e3da9305bc8359d4e5bdbca852deb4dfef
Rıdvan Akca <akca.ridvann@gmail.com>
2024-03-04 18:04:26 +0300

    fix(useAdminAppAuthClient): pass signal

--- packages/web/src/hooks/useAdminAppAuthClient.ee.js
+++ packages/web/src/hooks/useAdminAppAuthClient.ee.js
@@ -6,7 +6,9 @@ export default function useAdminAppAuthClient(id) {
   const query = useQuery({
     queryKey: ['adminAppAuthClient', id],
     queryFn: async ({ payload, signal }) => {
-      const { data } = await api.get(`/v1/admin/app-auth-clients/${id}`);
+      const { data } = await api.get(`/v1/admin/app-auth-clients/${id}`, {
+        signal,
+      });
 
       return data;
     },
