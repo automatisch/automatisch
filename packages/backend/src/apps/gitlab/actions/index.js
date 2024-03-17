b195e8a9c679d1a46d22cf562781b11f6c6c5fb4
Chris Qiang <chris@christianto.net>
2024-03-17 21:05:19 +0700

    fix(gitlab): properly group the codebases import

--- packages/backend/src/apps/gitlab/actions/index.js
+++ packages/backend/src/apps/gitlab/actions/index.js
@@ -1,19 +1,5 @@
-import issueCreate from './issues/create.js';
-import issueUpdate from './issues/update.js';
-import issueList from './issues/list.js';
-import issueGet from './issues/get.js';
-import issueLinkCreate from './issue-links/create.js';
-import issueLinkDelete from './issue-links/delete.js';
-import issueLinkList from './issue-links/list.js';
+import issues from './issues/index.js';
+import issueLinks from './issue-links/index.js';
 import notes from './notes/index.js';
 
-export default [
-  issueCreate,
-  issueGet,
-  issueLinkCreate,
-  issueLinkDelete,
-  issueLinkList,
-  issueList,
-  issueUpdate,
-  ...notes,
-];
+export default [...issues, ...issueLinks, ...notes];
