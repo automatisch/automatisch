39147879acce43f45211bf0dc36ef645df54775c
Chris Qiang <chris@christianto.net>
2024-03-17 20:47:37 +0700

    fix(gitlab): register the issue getter to the index too

--- packages/backend/src/apps/gitlab/actions/index.js
+++ packages/backend/src/apps/gitlab/actions/index.js
@@ -1,6 +1,7 @@
 import issueCreate from './issues/create.js';
 import issueUpdate from './issues/update.js';
 import issueList from './issues/list.js';
+import issueGet from './issues/get.js';
 import issueLinkCreate from './issue-links/create.js';
 import issueLinkDelete from './issue-links/delete.js';
 import issueLinkList from './issue-links/list.js';
@@ -8,6 +9,7 @@ import notes from './notes/index.js';
 
 export default [
   issueCreate,
+  issueGet,
   issueLinkCreate,
   issueLinkDelete,
   issueLinkList,
