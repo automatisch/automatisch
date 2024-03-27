8f444eafa763dd5f8c7af78ad5d1b6c47939742b
Faruk AYDIN <omerfaruk26@gmail.com>
2024-03-27 13:44:01 +0100

    chore: Remove old admin app auth clients API endpoint

--- packages/backend/src/routes/api/v1/admin/app-auth-clients.ee.js
+++ packages/backend/src/routes/api/v1/admin/app-auth-clients.ee.js
@@ -3,18 +3,9 @@ import asyncHandler from 'express-async-handler';
 import { authenticateUser } from '../../../../helpers/authentication.js';
 import { authorizeAdmin } from '../../../../helpers/authorization.js';
 import { checkIsEnterprise } from '../../../../helpers/check-is-enterprise.js';
-import getAdminAppAuthClientsAction from '../../../../controllers/api/v1/admin/app-auth-clients/get-app-auth-clients.ee.js';
 import getAdminAppAuthClientAction from '../../../../controllers/api/v1/admin/app-auth-clients/get-app-auth-client.ee.js';
 
 const router = Router();
-
-router.get(
-  '/',
-  authenticateUser,
-  authorizeAdmin,
-  checkIsEnterprise,
-  asyncHandler(getAdminAppAuthClientsAction)
-);
 
 router.get(
   '/:appAuthClientId',
