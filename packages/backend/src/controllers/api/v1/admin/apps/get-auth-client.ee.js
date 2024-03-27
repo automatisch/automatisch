4942cf8dae748aab257c63b4e2d360dd174e2e2f
Faruk AYDIN <omerfaruk26@gmail.com>
2024-03-27 13:59:10 +0100

    feat: Implement new admin get app auth client API endpoint

---
+++ packages/backend/src/controllers/api/v1/admin/apps/get-auth-client.ee.js
@@ -0,0 +1,11 @@
+import { renderObject } from '../../../../../helpers/renderer.js';
+import AppAuthClient from '../../../../../models/app-auth-client.js';
+
+export default async (request, response) => {
+  const appAuthClient = await AppAuthClient.query()
+    .findById(request.params.appAuthClientId)
+    .where({ app_key: request.params.appKey })
+    .throwIfNotFound();
+
+  renderObject(response, appAuthClient);
+};
--- packages/backend/src/controllers/api/v1/admin/apps/get-auth-client.ee.test.js
+++ packages/backend/src/controllers/api/v1/admin/apps/get-auth-client.ee.test.js
@@ -1,20 +1,23 @@
 import { vi, describe, it, expect, beforeEach } from 'vitest';
 import request from 'supertest';
 import Crypto from 'crypto';
-import app from '../../../../app.js';
-import createAuthTokenByUserId from '../../../../helpers/create-auth-token-by-user-id.js';
-import { createUser } from '../../../../../test/factories/user.js';
-import getAppAuthClientMock from '../../../../../test/mocks/rest/api/v1/admin/apps/get-auth-client.js';
-import { createAppAuthClient } from '../../../../../test/factories/app-auth-client.js';
-import * as license from '../../../../helpers/license.ee.js';
+import app from '../../../../../app.js';
+import createAuthTokenByUserId from '../../../../../helpers/create-auth-token-by-user-id.js';
+import { createUser } from '../../../../../../test/factories/user.js';
+import { createRole } from '../../../../../../test/factories/role.js';
+import getAppAuthClientMock from '../../../../../../test/mocks/rest/api/v1/admin/apps/get-auth-client.js';
+import { createAppAuthClient } from '../../../../../../test/factories/app-auth-client.js';
+import * as license from '../../../../../helpers/license.ee.js';
 
-describe('GET /api/v1/apps/:appKey/auth-clients/:appAuthClientId', () => {
-  let currentUser, currentAppAuthClient, token;
+describe('GET /api/v1/admin/apps/:appKey/auth-clients/:appAuthClientId', () => {
+  let currentUser, adminRole, currentAppAuthClient, token;
 
   beforeEach(async () => {
     vi.spyOn(license, 'hasValidLicense').mockResolvedValue(true);
 
-    currentUser = await createUser();
+    adminRole = await createRole({ key: 'admin' });
+    currentUser = await createUser({ roleId: adminRole.id });
+
     currentAppAuthClient = await createAppAuthClient({
       appKey: 'deepl',
     });
@@ -24,7 +27,7 @@ describe('GET /api/v1/apps/:appKey/auth-clients/:appAuthClientId', () => {
 
   it('should return specified app auth client', async () => {
     const response = await request(app)
-      .get(`/api/v1/apps/deepl/auth-clients/${currentAppAuthClient.id}`)
+      .get(`/api/v1/admin/apps/deepl/auth-clients/${currentAppAuthClient.id}`)
       .set('Authorization', token)
       .expect(200);
 
@@ -36,14 +39,16 @@ describe('GET /api/v1/apps/:appKey/auth-clients/:appAuthClientId', () => {
     const notExistingAppAuthClientUUID = Crypto.randomUUID();
 
     await request(app)
-      .get(`/api/v1/apps/deepl/auth-clients/${notExistingAppAuthClientUUID}`)
+      .get(
+        `/api/v1/admin/apps/deepl/auth-clients/${notExistingAppAuthClientUUID}`
+      )
       .set('Authorization', token)
       .expect(404);
   });
 
   it('should return bad request response for invalid UUID', async () => {
     await request(app)
-      .get('/api/v1/apps/deepl/auth-clients/invalidAppAuthClientUUID')
+      .get('/api/v1/admin/apps/deepl/auth-clients/invalidAppAuthClientUUID')
       .set('Authorization', token)
       .expect(400);
   });
