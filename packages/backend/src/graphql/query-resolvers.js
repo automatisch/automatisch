91458f91ef2c73bae4be36f428237ec2e7687154
kasia.oczkowska <katarzyna.oczkowska@theworkingroup.net>
2024-04-05 15:28:23 +0100

    refactor: rewrite useAuthClients with RQ

--- packages/backend/src/graphql/query-resolvers.js
+++ packages/backend/src/graphql/query-resolvers.js
@@ -1,6 +1,5 @@
 import getApp from './queries/get-app.js';
 import getAppAuthClient from './queries/get-app-auth-client.ee.js';
-import getAppAuthClients from './queries/get-app-auth-clients.ee.js';
 import getBillingAndUsage from './queries/get-billing-and-usage.ee.js';
 import getConnectedApps from './queries/get-connected-apps.js';
 import getDynamicData from './queries/get-dynamic-data.js';
@@ -11,7 +10,6 @@ import testConnection from './queries/test-connection.js';
 const queryResolvers = {
   getApp,
   getAppAuthClient,
-  getAppAuthClients,
   getBillingAndUsage,
   getConnectedApps,
   getDynamicData,
