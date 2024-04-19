79cdc6483c218a2e5e8e4f80ba4ea4783967e67f
Rıdvan Akca <akca.ridvann@gmail.com>
2024-04-19 11:27:55 +0200

    feat(firebase): add new child object in a firebase realtime database

--- packages/backend/src/apps/firebase/triggers/index.js
+++ packages/backend/src/apps/firebase/triggers/index.js
@@ -1,3 +1,7 @@
+import newChildObjectInFirebaseRealtimeDatabase from './new-child-object-in-a-firebase-realtime-database/index.js';
 import newDocumentsWithinFirestoreCollection from './new-documents-within-firestore-collection/index.js';
 
-export default [newDocumentsWithinFirestoreCollection];
+export default [
+  newChildObjectInFirebaseRealtimeDatabase,
+  newDocumentsWithinFirestoreCollection,
+];
