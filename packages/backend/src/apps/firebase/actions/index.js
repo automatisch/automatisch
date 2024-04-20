67208042e64071f5398ba25ec5a9e69b1084ce2d
Rıdvan Akca <akca.ridvann@gmail.com>
2024-04-20 13:26:26 +0200

    feat(firebase): add create firebase realtime database record action

--- packages/backend/src/apps/firebase/actions/index.js
+++ packages/backend/src/apps/firebase/actions/index.js
@@ -1,4 +1,9 @@
 import createCloudFirestoreDocument from './create-cloud-firestore-document/index.js';
+import createFirebaseRealtimeDatabaseRecord from './create-firebase-realtime-database-record/index.js';
 import findCloudFirestoreDocument from './find-cloud-firestore-document/index.js';
 
-export default [createCloudFirestoreDocument, findCloudFirestoreDocument];
+export default [
+  createCloudFirestoreDocument,
+  createFirebaseRealtimeDatabaseRecord,
+  findCloudFirestoreDocument,
+];
--- packages/docs/pages/apps/firebase/actions.md
+++ packages/docs/pages/apps/firebase/actions.md
@@ -3,6 +3,8 @@ favicon: /favicons/firebase.svg
 items:
   - name: Create Cloud Firestore document
     desc: Creates a new document within a Cloud Firestore collection.
+  - name: Create Firebase Realtime Database Record
+    desc: Creates a child object within your Firebase Realtime Database.
   - name: Find Cloud Firestore document
     desc: Finds a document within a collection.
 ---
