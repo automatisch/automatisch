2166a3220e45d75884552e2deea2d9e3a105a869
Ali BARIN <ali.barin53@gmail.com>
2024-04-26 12:02:38 +0000

    style: add break line before variable declaration and beforeEach

--- packages/backend/src/controllers/api/v1/access-tokens/revoke-access-token.test.js
+++ packages/backend/src/controllers/api/v1/access-tokens/revoke-access-token.test.js
@@ -7,6 +7,7 @@ import AccessToken from '../../../../models/access-token.js';
 
 describe('DELETE /api/v1/access-tokens/:token', () => {
   let token;
+
   beforeEach(async () => {
     const currentUser = await createUser({
       email: 'user@automatisch.io',
