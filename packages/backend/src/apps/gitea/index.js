5a433ad119f69af36758cfe70c5b64e5ca4adffc
Rıdvan Akca <akca.ridvann@gmail.com>
2024-04-28 14:42:25 +0200

    feat(gitea): add gitea integration

---
+++ packages/backend/src/apps/gitea/index.js
@@ -0,0 +1,17 @@
+import defineApp from '../../helpers/define-app.js';
+import addAuthHeader from './common/add-auth-header.js';
+import auth from './auth/index.js';
+import setBaseUrl from './common/set-base-url.js';
+
+export default defineApp({
+  name: 'Gitea',
+  key: 'gitea',
+  iconUrl: '{BASE_URL}/apps/gitea/assets/favicon.svg',
+  authDocUrl: '{DOCS_URL}/apps/gitea/connection',
+  supportsConnections: true,
+  baseUrl: '',
+  apiBaseUrl: '',
+  primaryColor: '609926',
+  beforeRequest: [setBaseUrl, addAuthHeader],
+  auth,
+});
