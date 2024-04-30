f5925cbf7c8c52a0c2135b5352398a9aee7c3875
Rıdvan Akca <akca.ridvann@gmail.com>
2024-04-30 16:38:08 +0200

    feat(gitea): add new pull requests trigger

--- packages/backend/src/apps/gitea/triggers/index.js
+++ packages/backend/src/apps/gitea/triggers/index.js
@@ -1,5 +1,6 @@
 import newIssues from './new-issues/index.js';
+import newPullRequests from './new-pull-requests/index.js';
 import newStargazers from './new-stargazers/index.js';
 import newWatchers from './new-watchers/index.js';
 
-export default [newIssues, newStargazers, newWatchers];
+export default [newIssues, newPullRequests, newStargazers, newWatchers];
