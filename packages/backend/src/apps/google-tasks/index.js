e398bb84d4f72101adbaccbcf50ee81ab38ef74b
Ali BARIN <ali.barin53@gmail.com>
2024-05-06 16:15:32 +0000

    fix(google-tasks): use DOCS_URL variable in auth doc url

--- packages/backend/src/apps/google-tasks/index.js
+++ packages/backend/src/apps/google-tasks/index.js
@@ -11,7 +11,7 @@ export default defineApp({
   baseUrl: 'https://calendar.google.com/calendar/u/0/r/tasks',
   apiBaseUrl: 'https://tasks.googleapis.com',
   iconUrl: '{BASE_URL}/apps/google-tasks/assets/favicon.svg',
-  authDocUrl: 'https://automatisch.io/docs/apps/google-tasks/connection',
+  authDocUrl: '{DOCS_URL}/apps/google-tasks/connection',
   primaryColor: '0066DA',
   supportsConnections: true,
   beforeRequest: [addAuthHeader],
