61dc4861dd213bd8bcdee16ff97e8dacaf2fe4c6
Rıdvan Akca <akca.ridvann@gmail.com>
2024-05-21 14:25:38 +0200

    feat(firefly-iii): add transaction updated trigger

---
+++ packages/backend/src/apps/firefly-iii/triggers/transaction-updated/index.js
@@ -0,0 +1,89 @@
+import Crypto from 'crypto';
+import defineTrigger from '../../../../helpers/define-trigger.js';
+
+export default defineTrigger({
+  name: 'Transaction updated',
+  key: 'transactionUpdated',
+  type: 'webhook',
+  description: 'Triggers when a transaction is updated.',
+  arguments: [
+    {
+      label: 'Title of the webhook',
+      key: 'title',
+      type: 'string',
+      required: false,
+      description: '',
+      variables: true,
+    },
+  ],
+
+  async run($) {
+    const dataItem = {
+      raw: $.request.body,
+      meta: {
+        internalId: Crypto.randomUUID(),
+      },
+    };
+
+    $.pushTriggerItem(dataItem);
+  },
+
+  async testRun($) {
+    const { data: transactions } = await $.http.get(`/api/v1/transactions`);
+
+    if (transactions.data.length === 0) {
+      return;
+    }
+
+    const { data: transaction } = await $.http.get(
+      `/api/v1/transactions/${transactions.data[0].id}`
+    );
+
+    const lastTransaction = transaction.data;
+
+    if (!lastTransaction) {
+      return;
+    }
+
+    const computedWebhookEvent = {
+      url: '',
+      uuid: Crypto.randomUUID(),
+      content: lastTransaction.attributes,
+      trigger: 'UPDATE_TRANSACTION',
+      user_id: lastTransaction.attributes.user,
+      version: '',
+      response: 'TRANSACTIONS',
+    };
+
+    const dataItem = {
+      raw: computedWebhookEvent,
+      meta: {
+        internalId: computedWebhookEvent.uuid,
+      },
+    };
+
+    $.pushTriggerItem(dataItem);
+  },
+
+  async registerHook($) {
+    const title = $.step.parameters.title;
+
+    const payload = {
+      active: true,
+      title: title || `Flow ID: ${$.flow.id}`,
+      trigger: 'UPDATE_TRANSACTION',
+      response: 'TRANSACTIONS',
+      delivery: 'JSON',
+      url: $.webhookUrl,
+    };
+
+    const response = await $.http.post('/api/v1/webhooks', payload);
+    const id = response.data.data.id;
+
+    await $.flow.setRemoteWebhookId(id);
+  },
+
+  async unregisterHook($) {
+    await $.http.delete(`/api/v1/webhooks/${$.flow.remoteWebhookId}`);
+  },
+});
--- packages/docs/pages/apps/firefly-iii/triggers.md
+++ packages/docs/pages/apps/firefly-iii/triggers.md
@@ -3,6 +3,8 @@ favicon: /favicons/firefly-iii.svg
 items:
   - name: Transaction created
     desc: Triggers when a new transaction is created.
+  - name: Transaction updated
+    desc: Triggers when a transaction is updated.
 ---
 
 <script setup>
