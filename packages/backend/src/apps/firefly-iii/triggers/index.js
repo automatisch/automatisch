ab750b941bf53126e60408d50e0301bf86646252
Rıdvan Akca <akca.ridvann@gmail.com>
2024-05-21 14:49:19 +0200

    feat(firefly-iii): add transaction deleted trigger

--- packages/backend/src/apps/firefly-iii/triggers/index.js
+++ packages/backend/src/apps/firefly-iii/triggers/index.js
@@ -1,4 +1,5 @@
 import transactionCreated from './transaction-created/index.js';
+import transactionDeleted from './transaction-deleted/index.js';
 import transactionUpdated from './transaction-updated/index.js';
 
-export default [transactionCreated, transactionUpdated];
+export default [transactionCreated, transactionDeleted, transactionUpdated];
