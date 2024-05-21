db084d12e48cc343a4a9cae7e3e9d6be66014696
Rıdvan Akca <akca.ridvann@gmail.com>
2024-05-21 14:25:38 +0200

    feat(firefly-iii): add transaction updated trigger

--- packages/backend/src/apps/firefly-iii/triggers/index.js
+++ packages/backend/src/apps/firefly-iii/triggers/index.js
@@ -1,3 +1,4 @@
 import transactionCreated from './transaction-created/index.js';
+import transactionUpdated from './transaction-updated/index.js';
 
-export default [transactionCreated];
+export default [transactionCreated, transactionUpdated];
