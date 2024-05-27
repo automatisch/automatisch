232e7833db9998485d45732ca1a34dc55d3c1b40
Rıdvan Akca <akca.ridvann@gmail.com>
2024-05-27 17:22:47 +0200

    feat(bluesky): add reply to post action

--- packages/backend/src/apps/bluesky/actions/index.js
+++ packages/backend/src/apps/bluesky/actions/index.js
@@ -1,4 +1,5 @@
 import createPost from './create-post/index.js';
+import replyToPost from './reply-to-post/index.js';
 import searchPostByUrl from './search-post-by-url/index.js';
 
-export default [createPost, searchPostByUrl];
+export default [createPost, replyToPost, searchPostByUrl];
--- packages/docs/pages/apps/bluesky/actions.md
+++ packages/docs/pages/apps/bluesky/actions.md
@@ -3,6 +3,8 @@ favicon: /favicons/bluesky.svg
 items:
   - name: Create post
     desc: Creates a new post.
+  - name: Reply to post
+    desc: Replies to a post.
   - name: Search post by url
     desc: Searches a post in a thread by url.
 ---
