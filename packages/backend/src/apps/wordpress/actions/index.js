3d4a9865fe67516690ea66da0a339c556ac79c34
Rıdvan Akca <akca.ridvann@gmail.com>
2024-05-29 14:28:53 +0200

    feat(wordpress): add create user action

--- packages/backend/src/apps/wordpress/actions/index.js
+++ packages/backend/src/apps/wordpress/actions/index.js
@@ -1,5 +1,6 @@
 import createPost from './create-post/index.js';
+import createUser from './create-user/index.js';
 import findPost from './find-post/index.js';
 import updatePost from './update-post/index.js';
 
-export default [createPost, findPost, updatePost];
+export default [createPost, createUser, findPost, updatePost];
