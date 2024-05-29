e2dd46b1bff2ab2c0fe6a56aaf4b5214eaa099bd
Rıdvan Akca <akca.ridvann@gmail.com>
2024-05-29 15:50:09 +0200

    feat(wordpress): add delete post action

--- packages/backend/src/apps/wordpress/actions/index.js
+++ packages/backend/src/apps/wordpress/actions/index.js
@@ -1,7 +1,15 @@
 import createPost from './create-post/index.js';
 import createUser from './create-user/index.js';
+import deletePost from './delete-post/index.js';
 import findPost from './find-post/index.js';
 import findUser from './find-user/index.js';
 import updatePost from './update-post/index.js';
 
-export default [createPost, createUser, findPost, findUser, updatePost];
+export default [
+  createPost,
+  createUser,
+  deletePost,
+  findPost,
+  findUser,
+  updatePost,
+];
