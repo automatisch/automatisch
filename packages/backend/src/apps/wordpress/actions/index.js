d1bf2335853d2a8ba146b112336aa5091c91c43f
Rıdvan Akca <akca.ridvann@gmail.com>
2024-05-29 15:19:53 +0200

    feat(wordpress): add find user action

--- packages/backend/src/apps/wordpress/actions/index.js
+++ packages/backend/src/apps/wordpress/actions/index.js
@@ -1,6 +1,7 @@
 import createPost from './create-post/index.js';
 import createUser from './create-user/index.js';
 import findPost from './find-post/index.js';
+import findUser from './find-user/index.js';
 import updatePost from './update-post/index.js';
 
-export default [createPost, createUser, findPost, updatePost];
+export default [createPost, createUser, findPost, findUser, updatePost];
