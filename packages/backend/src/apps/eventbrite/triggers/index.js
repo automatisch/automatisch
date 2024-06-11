f8885aae83f2b51c463c08304c08261572e5d44f
Rıdvan Akca <akca.ridvann@gmail.com>
2024-06-11 14:19:54 +0200

    feat(eventbrite): add updated attendee trigger

--- packages/backend/src/apps/eventbrite/triggers/index.js
+++ packages/backend/src/apps/eventbrite/triggers/index.js
@@ -1,5 +1,11 @@
 import newAttendeeCheckIn from './new-attendee-check-in/index.js';
 import newAttendeeRegistered from './new-attendee-registered/index.js';
 import newEvents from './new-events/index.js';
+import updatedAttendee from './updated-attendee/index.js';
 
-export default [newAttendeeCheckIn, newAttendeeRegistered, newEvents];
+export default [
+  updatedAttendee,
+  newAttendeeCheckIn,
+  newAttendeeRegistered,
+  newEvents,
+];
