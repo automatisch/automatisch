a87c76df40963900abaf8d0919f8219a766f9a30
Rıdvan Akca <akca.ridvann@gmail.com>
2024-06-12 13:20:46 +0200

    feat(surveymonkey): add surveymonkey integration

--- packages/docs/pages/.vitepress/config.js
+++ packages/docs/pages/.vitepress/config.js
@@ -438,6 +438,14 @@ export default defineConfig({
           ],
         },
         {
+          text: 'SurveyMonkey',
+          collapsible: true,
+          collapsed: true,
+          items: [
+            { text: 'Connection', link: '/apps/surveymonkey/connection' },
+          ],
+        },
+        {
           text: 'Telegram',
           collapsible: true,
           collapsed: true,
