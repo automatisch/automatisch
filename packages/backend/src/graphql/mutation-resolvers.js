dd0a1328e841e7eccbe706b83ee35dba707126e7
kasia.oczkowska <katarzyna.oczkowska@theworkingroup.net>
2024-07-17 15:04:15 +0100

    feat: refactor reset password mutation with the REST API endpoint

--- packages/backend/src/graphql/mutation-resolvers.js
+++ packages/backend/src/graphql/mutation-resolvers.js
@@ -32,7 +32,6 @@ import verifyConnection from './mutations/verify-connection.js';
 // Converted mutations
 import deleteUser from './mutations/delete-user.ee.js';
 import login from './mutations/login.js';
-import resetPassword from './mutations/reset-password.ee.js';
 
 const mutationResolvers = {
   createAppAuthClient,
@@ -54,7 +53,6 @@ const mutationResolvers = {
   login,
   registerUser,
   resetConnection,
-  resetPassword,
   updateAppAuthClient,
   updateAppConfig,
   updateConfig,
