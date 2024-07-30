66f9cb8d25209defc1746486f3330da5047c4775
Ali BARIN <ali.barin53@gmail.com>
2024-07-30 18:31:57 +0000

    feat: support redis sentinel

--- packages/backend/src/config/redis.js
+++ packages/backend/src/config/redis.js
@@ -1,13 +1,29 @@
 import appConfig from './app.js';
 
 const redisConfig = {
-  host: appConfig.redisHost,
-  port: appConfig.redisPort,
   username: appConfig.redisUsername,
   password: appConfig.redisPassword,
+  db: appConfig.redisDb,
   enableOfflineQueue: false,
   enableReadyCheck: true,
 };
+
+if (appConfig.redisSentinelHost) {
+  redisConfig.sentinels = [
+    {
+      host: appConfig.redisSentinelHost,
+      port: appConfig.redisSentinelPort,
+    }
+  ];
+
+  redisConfig.sentinelUsername = appConfig.redisSentinelUsername;
+  redisConfig.sentinelPassword = appConfig.redisSentinelPassword;
+  redisConfig.name = appConfig.redisName;
+  redisConfig.role = appConfig.redisRole;
+} else {
+  redisConfig.host = appConfig.redisHost;
+  redisConfig.port = appConfig.redisPort;
+}
 
 if (appConfig.redisTls) {
   redisConfig.tls = {};
