7506bf186b9ee23dd7a5982d48a072f1898cb965
Ali BARIN <ali.barin53@gmail.com>
2024-08-14 14:12:49 +0000

    feat(jotform): prettify screen name

--- packages/backend/src/apps/jotform/auth/verify-credentials.js
+++ packages/backend/src/apps/jotform/auth/verify-credentials.js
@@ -3,10 +3,8 @@ import getCurrentUser from '../common/get-current-user.js';
 const verifyCredentials = async ($) => {
   const user = await getCurrentUser($);
 
-  const screenName = [user.username, user.email].filter(Boolean).join(' @ ');
-
   await $.auth.set({
-    screenName,
+    screenName: user.name,
     apiKey: $.auth.data.apiKey,
   });
 };
