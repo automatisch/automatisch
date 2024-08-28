4054f551d4cb0d8a98dbab1ad032f5c84ee3ceb1
Faruk AYDIN <omerfaruk26@gmail.com>
2024-08-28 17:07:13 +0300

    feat: Implement update user rest API endpoint

--- packages/backend/src/routes/api/v1/users.js
+++ packages/backend/src/routes/api/v1/users.js
@@ -3,6 +3,7 @@ import { authenticateUser } from '../../../helpers/authentication.js';
 import { authorizeUser } from '../../../helpers/authorization.js';
 import checkIsCloud from '../../../helpers/check-is-cloud.js';
 import getCurrentUserAction from '../../../controllers/api/v1/users/get-current-user.js';
+import updateCurrentUserAction from '../../../controllers/api/v1/users/update-current-user.js';
 import getUserTrialAction from '../../../controllers/api/v1/users/get-user-trial.ee.js';
 import getAppsAction from '../../../controllers/api/v1/users/get-apps.js';
 import getInvoicesAction from '../../../controllers/api/v1/users/get-invoices.ee.js';
@@ -15,6 +16,7 @@ import resetPasswordAction from '../../../controllers/api/v1/users/reset-passwor
 const router = Router();
 
 router.get('/me', authenticateUser, getCurrentUserAction);
+router.patch('/:userId', authenticateUser, updateCurrentUserAction);
 router.get('/:userId/apps', authenticateUser, authorizeUser, getAppsAction);
 router.get('/invoices', authenticateUser, checkIsCloud, getInvoicesAction);
 
