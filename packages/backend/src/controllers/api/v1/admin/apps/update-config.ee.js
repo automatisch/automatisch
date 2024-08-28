ca81e14d6397652e8157b45249f485f485f2a9ba
Ali BARIN <ali.barin53@gmail.com>
2024-08-28 12:19:57 +0000

    feat: write PATCH /v1/admin/apps/:appKey/config

---
+++ packages/backend/src/controllers/api/v1/admin/apps/update-config.ee.js
@@ -0,0 +1,24 @@
+import { renderObject } from '../../../../../helpers/renderer.js';
+import AppConfig from '../../../../../models/app-config.js';
+
+export default async (request, response) => {
+  const appConfig = await AppConfig.query()
+    .findOne({
+      key: request.params.appKey,
+    })
+    .throwIfNotFound();
+
+  await appConfig.$query().patchAndFetch(appConfigParams(request));
+
+  renderObject(response, appConfig);
+};
+
+const appConfigParams = (request) => {
+  const { allowCustomConnection, shared, disabled } = request.body;
+
+  return {
+    allowCustomConnection,
+    shared,
+    disabled,
+  };
+};
