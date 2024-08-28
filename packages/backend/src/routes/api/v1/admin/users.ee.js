d0aa2bca69c3cb62c25a5c82b95c4463ac82bcc1
Faruk AYDIN <omerfaruk26@gmail.com>
2024-08-28 17:49:38 +0300

    feat: Implement rest API endpoint to update users for admin

--- packages/backend/src/routes/api/v1/admin/users.ee.js
+++ packages/backend/src/routes/api/v1/admin/users.ee.js
@@ -3,12 +3,14 @@ import { authenticateUser } from '../../../../helpers/authentication.js';
 import { authorizeAdmin } from '../../../../helpers/authorization.js';
 import getUsersAction from '../../../../controllers/api/v1/admin/users/get-users.ee.js';
 import getUserAction from '../../../../controllers/api/v1/admin/users/get-user.ee.js';
+import updateUserAction from '../../../../controllers/api/v1/admin/users/update-user.ee.js';
 import deleteUserAction from '../../../../controllers/api/v1/admin/users/delete-user.js';
 
 const router = Router();
 
 router.get('/', authenticateUser, authorizeAdmin, getUsersAction);
 router.get('/:userId', authenticateUser, authorizeAdmin, getUserAction);
+router.patch('/:userId', authenticateUser, authorizeAdmin, updateUserAction);
 router.delete('/:userId', authenticateUser, authorizeAdmin, deleteUserAction);
 
 export default router;
