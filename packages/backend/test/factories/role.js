b089069b8e961f307f33fb84bf4c902f3b4a2c35
Ali BARIN <ali.barin53@gmail.com>
2024-09-03 14:18:14 +0000

    test: use faker in role factory

--- packages/backend/test/factories/role.js
+++ packages/backend/test/factories/role.js
@@ -1,8 +1,12 @@
+import { faker } from '@faker-js/faker';
 import Role from '../../src/models/role';
 
 export const createRole = async (params = {}) => {
-  params.name = params?.name || 'Viewer';
-  params.key = params?.key || 'viewer';
+  const name = faker.lorem.word();
+  const key = name.toLowerCase();
+
+  params.name = params?.name || name;
+  params.key = params?.key || key;
 
   const role = await Role.query().insertAndFetch(params);
 
