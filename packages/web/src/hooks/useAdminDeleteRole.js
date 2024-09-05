66fe84e12666242b8cafa6659ccac74a407b74d3
Ali BARIN <ali.barin53@gmail.com>
2024-09-05 15:31:50 +0000

    feat(DeleteRoleButton): use REST API endpoint to delete role

---
+++ packages/web/src/hooks/useAdminDeleteRole.js
@@ -0,0 +1,25 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import api from 'helpers/api';
+
+export default function useAdminDeleteRole(roleId) {
+  const queryClient = useQueryClient();
+
+  const query = useMutation({
+    mutationFn: async (payload) => {
+      const { data } = await api.delete(`/v1/admin/roles/${roleId}`);
+
+      return data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['admin', 'roles'],
+      });
+
+      queryClient.invalidateQueries({
+        queryKey: ['admin', 'roles', roleId],
+      });
+    },
+  });
+
+  return query;
+}
