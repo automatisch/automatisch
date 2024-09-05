06c4b7ed2e04395aad653dc47827677c43594dd0
Faruk AYDIN <omerfaruk26@gmail.com>
2024-09-05 13:32:19 +0300

    fix: Throw error if trigger doesn't have a type

--- packages/backend/src/helpers/define-trigger.js
+++ packages/backend/src/helpers/define-trigger.js
@@ -1,3 +1,27 @@
+import logger from './logger.js';
+
 export default function defineTrigger(triggerDefinition) {
+  const isWebhookOrPoll =
+    triggerDefinition.pollInterval || triggerDefinition.type === 'webhook';
+
+  const schedulerTriggers = [
+    'everyHour',
+    'everyDay',
+    'everyWeek',
+    'everyMonth',
+  ];
+
+  const isSchedulerTrigger = schedulerTriggers.includes(triggerDefinition.key);
+
+  const haveValidTriggerType = isWebhookOrPoll || isSchedulerTrigger;
+
+  if (!haveValidTriggerType) {
+    logger.info(triggerDefinition);
+
+    throw new Error(
+      `Trigger must have a poll interval or be a webhook for ${triggerDefinition.key}`
+    );
+  }
+
   return triggerDefinition;
 }
