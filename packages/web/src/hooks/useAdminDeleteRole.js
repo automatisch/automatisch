61a1ce57c24b0788799ce316831b11baa8053a7a
Ali BARIN <ali.barin53@gmail.com>
2024-09-06 09:52:03 +0000

    refactor(useAdminDeleteRole): remove redundant invalidateQueries

--- packages/web/src/hooks/useAdminDeleteRole.js
+++ packages/web/src/hooks/useAdminDeleteRole.js
@@ -14,10 +14,6 @@ export default function useAdminDeleteRole(roleId) {
       queryClient.invalidateQueries({
         queryKey: ['admin', 'roles'],
       });
-
-      queryClient.invalidateQueries({
-        queryKey: ['admin', 'roles', roleId],
-      });
     },
   });
 
