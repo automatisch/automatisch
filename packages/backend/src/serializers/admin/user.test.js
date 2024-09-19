c688d67e4b752992cc7cf3b5066d36f74e085a9b
Faruk AYDIN <omerfaruk26@gmail.com>
2024-09-19 13:48:08 +0300

    feat: Implement admin user serializer test

---
+++ packages/backend/src/serializers/admin/user.test.js
@@ -0,0 +1,19 @@
+import { describe, it, expect, beforeEach } from 'vitest';
+import { createUser } from '../../../test/factories/user';
+import adminUserSerializer from './user.js';
+
+describe('adminUserSerializer', () => {
+  let user;
+
+  beforeEach(async () => {
+    user = await createUser();
+  });
+
+  it('should return user data with accept invitation url', async () => {
+    const serializedUser = adminUserSerializer(user);
+
+    expect(serializedUser.acceptInvitationUrl).toEqual(
+      user.acceptInvitationUrl
+    );
+  });
+});
