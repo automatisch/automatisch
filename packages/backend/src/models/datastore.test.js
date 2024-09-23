9006a0c25f153241da5acc4c9b446dfceccc8698
Faruk AYDIN <omerfaruk26@gmail.com>
2024-09-23 16:32:36 +0300

    refactor: Update required validations with custom assertion

--- packages/backend/src/models/datastore.test.js
+++ packages/backend/src/models/datastore.test.js
@@ -6,23 +6,9 @@ describe('Datastore model', () => {
     expect(Datastore.tableName).toBe('datastore');
   });
 
-  it('jsonSchema should have the correct schema', () => {
-    const expectedSchema = {
-      type: 'object',
-      required: ['key', 'value', 'scope', 'scopeId'],
-      properties: {
-        id: { type: 'string', format: 'uuid' },
-        key: { type: 'string', minLength: 1 },
-        value: { type: 'string' },
-        scope: {
-          type: 'string',
-          enum: ['flow'],
-          default: 'flow',
-        },
-        scopeId: { type: 'string', format: 'uuid' },
-      },
-    };
-
-    expect(Datastore.jsonSchema).toStrictEqual(expectedSchema);
+  it('jsonSchema should have correct validations', () => {
+    expect(Datastore).toRequireProperty('key');
+    expect(Datastore).toRequireProperty('value');
+    expect(Datastore).toRequireProperty('scopeId');
   });
 });
