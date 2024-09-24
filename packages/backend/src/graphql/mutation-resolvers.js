23c1a42163df4b47726eeabc0c8a4a0967dc1592
Ali BARIN <ali.barin53@gmail.com>
2024-09-24 08:30:07 +0000

    chore: remove redundant create connection mutation

--- packages/backend/src/graphql/mutation-resolvers.js
+++ packages/backend/src/graphql/mutation-resolvers.js
@@ -2,12 +2,10 @@
 import verifyConnection from './mutations/verify-connection.js';
 import updateCurrentUser from './mutations/update-current-user.js';
 import generateAuthUrl from './mutations/generate-auth-url.js';
-import createConnection from './mutations/create-connection.js';
 import resetConnection from './mutations/reset-connection.js';
 import updateConnection from './mutations/update-connection.js';
 
 const mutationResolvers = {
-  createConnection,
   generateAuthUrl,
   resetConnection,
   updateConnection,
