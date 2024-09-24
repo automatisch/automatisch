dc0273148cd078c1ec3530fc79f672fd26a2a852
Ali BARIN <ali.barin53@gmail.com>
2024-09-24 11:44:42 +0000

    chore: remove redundant reset connection mutation

--- packages/backend/src/graphql/mutation-resolvers.js
+++ packages/backend/src/graphql/mutation-resolvers.js
@@ -1,14 +1,7 @@
 // Converted mutations
 import verifyConnection from './mutations/verify-connection.js';
-import resetConnection from './mutations/reset-connection.js';
 
 const mutationResolvers = {
-  resetConnection,
-<<<<<<< HEAD
-  updateConnection,
-=======
-  updateCurrentUser,
->>>>>>> 82cde73e (chore: remove redundant update connection mutation)
   verifyConnection,
 };
 
