759a729b43aacb3103a073cabec3640bb7daa1a0
Ali BARIN <ali.barin53@gmail.com>
2024-09-24 11:44:42 +0000

    chore: remove redundant reset connection mutation

--- packages/web/src/graphql/mutations/index.js
+++ packages/web/src/graphql/mutations/index.js
@@ -1,9 +1,7 @@
 import { VERIFY_CONNECTION } from './verify-connection';
-import { RESET_CONNECTION } from './reset-connection';
 
 const mutations = {
   verifyConnection: VERIFY_CONNECTION,
-  resetConnection: RESET_CONNECTION,
 };
 
 export default mutations;
