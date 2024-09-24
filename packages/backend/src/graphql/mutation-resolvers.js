3c7a31c22cc46ff7975b1225e1c6e1d546551cba
Ali BARIN <ali.barin53@gmail.com>
2024-09-24 09:46:56 +0000

    chore: remove redundant generate auth url mutation

--- packages/backend/src/graphql/mutation-resolvers.js
+++ packages/backend/src/graphql/mutation-resolvers.js
@@ -1,12 +1,10 @@
 // Converted mutations
 import verifyConnection from './mutations/verify-connection.js';
 import updateCurrentUser from './mutations/update-current-user.js';
-import generateAuthUrl from './mutations/generate-auth-url.js';
 import resetConnection from './mutations/reset-connection.js';
 import updateConnection from './mutations/update-connection.js';
 
 const mutationResolvers = {
-  generateAuthUrl,
   resetConnection,
   updateConnection,
   updateCurrentUser,
--- packages/web/src/graphql/mutations/index.js
+++ packages/web/src/graphql/mutations/index.js
@@ -1,12 +1,11 @@
 import { UPDATE_CONNECTION } from './update-connection';
 import { VERIFY_CONNECTION } from './verify-connection';
 import { RESET_CONNECTION } from './reset-connection';
-import { GENERATE_AUTH_URL } from './generate-auth-url';
 
 const mutations = {
   updateConnection: UPDATE_CONNECTION,
   verifyConnection: VERIFY_CONNECTION,
   resetConnection: RESET_CONNECTION,
-  generateAuthUrl: GENERATE_AUTH_URL,
 };
+
 export default mutations;
