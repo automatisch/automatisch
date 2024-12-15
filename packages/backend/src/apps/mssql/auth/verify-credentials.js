49373a3c4fadbb0c8dfb061c0e05ebb2647362c9
Yash Soni <yash.soni2737@gmail.com>
2024-12-16 00:04:45 +0530

    WIP: add mssql connector

---
+++ packages/backend/src/apps/mssql/auth/verify-credentials.js
@@ -0,0 +1,24 @@
+import logger from '../../../helpers/logger.js';
+import getClient from '../common/mssql-client.js';
+
+const verifyCredentials = async ($) => {
+  const client = getClient($);
+  const checkConnection = await client.raw('SELECT 1');
+  client.destroy();
+
+  logger.debug(checkConnection);
+
+  await $.auth.set({
+    screenName: `${$.auth.data.user}@${$.auth.data.host}:${$.auth.data.port}/${$.auth.data.database}`,
+    client: 'mssql',
+    host: $.auth.data.host,
+    port: Number($.auth.data.port),
+    enableSsl:
+      $.auth.data.enableSsl === 'true' || $.auth.data.enableSsl === true,
+    user: $.auth.data.user,
+    password: $.auth.data.password,
+    database: $.auth.data.database,
+  });
+};
+
+export default verifyCredentials;
