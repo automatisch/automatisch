b1291d018530380cce99c91a6015aaadc8a5ba43
Ali BARIN <ali.barin53@gmail.com>
2024-12-19 15:46:25 +0000

    feat(signalwire): add dynamic data for nodes and attributes

--- packages/backend/src/apps/signalwire/dynamic-data/index.js
+++ packages/backend/src/apps/signalwire/dynamic-data/index.js
@@ -1,4 +1,11 @@
-import listIncomingSmsPhoneNumbers from './list-incoming-sms-phone-numbers/index.js';
 import listIncomingCallPhoneNumbers from './list-incoming-call-phone-numbers/index.js';
+import listIncomingSmsPhoneNumbers from './list-incoming-sms-phone-numbers/index.js';
+import listVoiceXmlNodeAttributes from './list-voice-xml-node-attributess/index.js';
+import listVoiceXmlNodes from './list-voice-xml-nodes/index.js';
 
-export default [listIncomingCallPhoneNumbers, listIncomingSmsPhoneNumbers];
+export default [
+  listIncomingCallPhoneNumbers,
+  listIncomingSmsPhoneNumbers,
+  listVoiceXmlNodeAttributes,
+  listVoiceXmlNodes,
+];
