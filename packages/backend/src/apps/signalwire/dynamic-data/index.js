4392a1bf93abe38cdd447ea34b677bd53ea24f07
Ali BARIN <ali.barin53@gmail.com>
2025-01-09 14:15:00 +0000

    chore(signalwire): fix typo in folder name

--- packages/backend/src/apps/signalwire/dynamic-data/index.js
+++ packages/backend/src/apps/signalwire/dynamic-data/index.js
@@ -1,6 +1,6 @@
 import listIncomingCallPhoneNumbers from './list-incoming-call-phone-numbers/index.js';
 import listIncomingSmsPhoneNumbers from './list-incoming-sms-phone-numbers/index.js';
-import listVoiceXmlNodeAttributes from './list-voice-xml-node-attributess/index.js';
+import listVoiceXmlNodeAttributes from './list-voice-xml-node-attributes/index.js';
 import listVoiceXmlNodes from './list-voice-xml-nodes/index.js';
 
 export default [
