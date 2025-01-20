1db0ccb0a1cdf9593ab9263be5d342de6c582f64
Ali BARIN <ali.barin53@gmail.com>
2025-01-20 12:27:09 +0000

    feat(web): add import flow functionality

--- packages/web/src/pages/Flow/index.jsx
+++ packages/web/src/pages/Flow/index.jsx
@@ -2,9 +2,12 @@ import * as React from 'react';
 import { useParams } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+
 import Container from 'components/Container';
+
 export default function Flow() {
   const { flowId } = useParams();
+
   return (
     <Box sx={{ py: 3 }}>
       <Container>
