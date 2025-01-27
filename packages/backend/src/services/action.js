f168f48efb906e74f99ec1acfa01ac2f291bf52c
Ali BARIN <ali.barin53@gmail.com>
2025-01-27 16:17:34 +0000

    fix(services/action): log after assigning error to prevent mutation

--- packages/backend/src/services/action.js
+++ packages/backend/src/services/action.js
@@ -51,8 +51,6 @@ export const processAction = async (options) => {
     const shouldNotConsiderAsError = shouldEarlyExit || shouldNotProcess;
 
     if (!shouldNotConsiderAsError) {
-      logger.error(error);
-
       if (error instanceof HttpError) {
         $.actionOutput.error = error.details;
       } else {
@@ -62,6 +60,8 @@ export const processAction = async (options) => {
           $.actionOutput.error = { error: error.message };
         }
       }
+
+      logger.error(error);
     }
   }
 
