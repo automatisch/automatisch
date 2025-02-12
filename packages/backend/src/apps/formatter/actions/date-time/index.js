b61c6807137dcbb861f5c7cac6ea88bd156243b1
Ali BARIN <ali.barin53@gmail.com>
2025-02-12 11:47:25 +0000

    feat(formatter/date-time): add get current timestamp in seconds

--- packages/backend/src/apps/formatter/actions/date-time/index.js
+++ packages/backend/src/apps/formatter/actions/date-time/index.js
@@ -1,10 +1,12 @@
 import defineAction from '../../../../helpers/define-action.js';
 import formatDateTime from './transformers/format-date-time.js';
 import getCurrentTimestamp from './transformers/get-current-timestamp.js';
+import getCurrentTimestampInSeconds from './transformers/get-current-timestamp-in-seconds.js';
 
 const transformers = {
   formatDateTime,
   getCurrentTimestamp,
+  getCurrentTimestampInSeconds,
 };
 
 export default defineAction({
@@ -22,6 +24,10 @@ export default defineAction({
         {
           label: 'Get current timestamp',
           value: 'getCurrentTimestamp',
+        },
+        {
+          label: 'Get current timestamp in seconds',
+          value: 'getCurrentTimestampInSeconds',
         },
         {
           label: 'Format Date / Time',
---
+++ packages/backend/src/apps/formatter/actions/date-time/transformers/get-current-timestamp-in-seconds.js
@@ -0,0 +1,5 @@
+const getCurrentTimestampInSeconds = () => {
+  return Math.floor(Date.now() / 1000);
+};
+
+export default getCurrentTimestampInSeconds;
