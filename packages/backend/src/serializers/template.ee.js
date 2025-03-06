4580027e60912f19b565930b0d8fb55f71c824b2
Faruk AYDIN <omerfaruk26@gmail.com>
2025-03-06 17:35:10 +0100

    feat: Add icon urls to template serializer

--- packages/backend/src/serializers/template.ee.js
+++ packages/backend/src/serializers/template.ee.js
@@ -1,8 +1,18 @@
+import { generateIconUrl } from '../helpers/generate-icon-url.js';
+
 const templateSerializer = (template) => {
+  const flowDataWithIconUrls = {
+    ...template.flowData,
+    steps: template.flowData.steps?.map((step) => ({
+      ...step,
+      iconUrl: generateIconUrl(step.appKey),
+    })),
+  };
+
   return {
     id: template.id,
     name: template.name,
-    flowData: template.flowData,
+    flowData: flowDataWithIconUrls,
     createdAt: template.createdAt.getTime(),
     updatedAt: template.updatedAt.getTime(),
   };
