417aa3bd3035257730422470abe061c489e8c46f
Faruk AYDIN <omerfaruk26@gmail.com>
2025-03-19 16:44:24 +0100

    feat: Update execution status after the creation of execution step

--- packages/backend/src/models/execution-step.js
+++ packages/backend/src/models/execution-step.js
@@ -68,10 +68,19 @@ class ExecutionStep extends Base {
     }
   }
 
+  async updateExecutionStatus() {
+    const execution = await this.$relatedQuery('execution');
+
+    await execution.$query().patch({
+      status: this.status === 'failure' ? 'failure' : 'success',
+    });
+  }
+
   async $afterInsert(queryContext) {
     await super.$afterInsert(queryContext);
     Telemetry.executionStepCreated(this);
     await this.increaseUsageCount();
+    await this.updateExecutionStatus();
   }
 }
 
