ee1b910f3c4aacdfb80de5d9411b6746b187b8dc
Ali BARIN <ali.barin53@gmail.com>
2025-04-02 10:26:28 +0000

    feat(backend): use manage permissions in checks

--- packages/backend/src/helpers/permission-catalog.ee.js
+++ packages/backend/src/helpers/permission-catalog.ee.js
@@ -17,56 +17,22 @@ const permissionCatalog = {
   conditions: [
     {
       key: 'isCreator',
-      label: 'Is creator'
-    }
+      label: 'Is creator',
+    },
   ],
   actions: [
     {
-      label: 'Create',
-      key: 'create',
-      subjects: [
-        Connection.key,
-        Flow.key,
-      ]
+      label: 'Manage',
+      key: 'manage',
+      subjects: [Connection.key, Flow.key],
     },
     {
       label: 'Read',
       key: 'read',
-      subjects: [
-        Connection.key,
-        Execution.key,
-        Flow.key,
-      ]
+      subjects: [Connection.key, Execution.key, Flow.key],
     },
-    {
-      label: 'Update',
-      key: 'update',
-      subjects: [
-        Connection.key,
-        Flow.key,
-      ]
-    },
-    {
-      label: 'Delete',
-      key: 'delete',
-      subjects: [
-        Connection.key,
-        Flow.key,
-      ]
-    },
-    {
-      label: 'Publish',
-      key: 'publish',
-      subjects: [
-        Flow.key,
-      ]
-    }
   ],
-  subjects: [
-    Connection,
-    Flow,
-    Execution
-  ]
+  subjects: [Connection, Flow, Execution],
 };
 
 export default permissionCatalog;
