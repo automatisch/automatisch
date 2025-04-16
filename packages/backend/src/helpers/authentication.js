883736636b6a6c2647b038a829fd456d00a6cabe
Faruk AYDIN <omerfaruk26@gmail.com>
2025-04-16 11:09:39 +0200

    feat: Implement authentication for API tokens

--- packages/backend/src/helpers/authentication.js
+++ packages/backend/src/helpers/authentication.js
@@ -1,5 +1,6 @@
 import User from '../models/user.js';
 import AccessToken from '../models/access-token.js';
+import ApiToken from '../models/api-token.ee.js';
 
 export const isAuthenticated = async (req) => {
   const token = req.headers['authorization'];
@@ -41,6 +42,32 @@ export const isAuthenticated = async (req) => {
 
 export const authenticateUser = async (request, response, next) => {
   if (await isAuthenticated(request)) {
+    next();
+  } else {
+    return response.status(401).end();
+  }
+};
+
+export const isApiTokenAuthenticated = async (request) => {
+  const token = request.headers['x-api-token'];
+
+  if (token == null) return false;
+
+  try {
+    const apiToken = await ApiToken.query().findOne({
+      token,
+    });
+
+    if (apiToken == null) return false;
+
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+export const authenticateApiToken = async (request, response, next) => {
+  if (await isApiTokenAuthenticated(request)) {
     next();
   } else {
     return response.status(401).end();
