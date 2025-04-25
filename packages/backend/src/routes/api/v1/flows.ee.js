13f29554a92290fc4e068f5475cf8185cc7ce18b
Ali BARIN <ali.barin53@gmail.com>
2025-04-25 08:31:36 +0000

    feat(api): add delete flow endpoint

--- packages/backend/src/routes/api/v1/flows.ee.js
+++ packages/backend/src/routes/api/v1/flows.ee.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import getFlowAction from '../../../controllers/api/v1/flows/get-flow.ee.js';
+import deleteFlowAction from '../../../controllers/api/v1/flows/delete-flow.ee.js';
 
 const router = Router();
 
 router.get('/:flowId', getFlowAction);
+router.delete('/:flowId', deleteFlowAction);
 
 export default router;
