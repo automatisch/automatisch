279d9ca859e97de326015ac00e6238b3ed0863ed
Ali BARIN <ali.barin53@gmail.com>
2025-04-25 12:43:48 +0000

    feat(api): add delete user endpoint

--- packages/backend/src/routes/api/v1/users.ee.js
+++ packages/backend/src/routes/api/v1/users.ee.js
@@ -3,12 +3,14 @@ import createFlowAction from '../../../controllers/api/v1/users/create-flow.ee.j
 import createFolderAction from '../../../controllers/api/v1/users/create-folder.ee.js';
 import getFoldersAction from '../../../controllers/api/v1/users/get-folders.ee.js';
 import getUserAction from '../../../controllers/api/v1/users/get-user.ee.js';
+import deleteUserAction from '../../../controllers/api/v1/users/delete-user.ee.js';
 import getUsersAction from '../../../controllers/api/v1/users/get-users.ee.js';
 
 const router = Router();
 
 router.get('/', getUsersAction);
 router.get('/:userId', getUserAction);
+router.delete('/:userId', deleteUserAction);
 router.post('/:userId/flows', createFlowAction);
 router.get('/:userId/folders', getFoldersAction);
 router.post('/:userId/folders', createFolderAction);
