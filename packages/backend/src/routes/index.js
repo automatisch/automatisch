c8b14ec7e41b806f859768ea71bd39f66f3824d0
Ali BARIN <ali.barin53@gmail.com>
2025-05-09 13:44:30 +0000

    feat(api): introduce openapi.json

--- packages/backend/src/routes/index.js
+++ packages/backend/src/routes/index.js
@@ -4,15 +4,13 @@ import paddleRouter from '@/routes/paddle.ee.js';
 import healthcheckRouter from '@/routes/healthcheck.js';
 import apiRouter from '@/routes/api/index.js';
 import internalApiRouter from '@/routes/internal/api/index.js';
-import { checkIsEnterprise } from '@/helpers/check-is-enterprise.js';
-import { authenticateApiToken } from '@/helpers/authenticate-api-token.ee.js';
 
 const router = Router();
 
 router.use('/webhooks', webhooksRouter);
 router.use('/paddle', paddleRouter);
 router.use('/healthcheck', healthcheckRouter);
-router.use('/api', checkIsEnterprise, authenticateApiToken, apiRouter);
+router.use('/api', apiRouter);
 router.use('/internal/api', internalApiRouter);
 
 export default router;
