7c985093b2b13f2a8ff466cdca6689a0ecd0b749
Faruk AYDIN <omerfaruk26@gmail.com>
2025-05-21 12:06:34 +0200

    refactor: Use renderError helper method for create form submission action

--- packages/backend/src/controllers/internal/api/v1/forms/create-form-submission.ee.js
+++ packages/backend/src/controllers/internal/api/v1/forms/create-form-submission.ee.js
@@ -1,6 +1,7 @@
 import Flow from '../../../../../models/flow.js';
 import logger from '../../../../../helpers/logger.js';
 import handler from '../../../../../helpers/form-handler.ee.js';
+import { renderError } from '../../../../../helpers/renderer.js';
 
 export default async (request, response) => {
   logger.debug(`Handling incoming form submission at ${request.originalUrl}.`);
@@ -11,8 +12,7 @@ export default async (request, response) => {
   const triggerStep = await flow.getTriggerStep();
 
   if (triggerStep.appKey !== 'forms') {
-    logger.error('Invalid trigger step');
-    return response.status(400).send('Invalid trigger step');
+    return renderError(response, [{ general: ['Invalid trigger step'] }], 400);
   }
 
   await handler(formId, request, response);
