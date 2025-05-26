e4c0f33862160f2d03a238d91c845adc368984ce
Faruk AYDIN <omerfaruk26@gmail.com>
2025-05-26 15:24:23 +0200

    chore: Remove form submission API endpoint

--- packages/backend/src/routes/internal/api/v1/forms.ee.js
+++ packages/backend/src/routes/internal/api/v1/forms.ee.js
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import getFormAction from '../../../../controllers/internal/api/v1/forms/get-form.ee.js';
 import useCreateForm from '../../../../controllers/internal/api/v1/forms/create-form.ee.js';
-import useCreateFormSubmission from '../../../../controllers/internal/api/v1/forms/create-form-submission.ee.js';
 import { authenticateUser } from '../../../../helpers/authentication.js';
 import { authorizeUser } from '../../../../helpers/authorization.js';
 
@@ -9,6 +8,5 @@ const router = Router();
 
 router.get('/:formId', authenticateUser, authorizeUser, getFormAction);
 router.post('/', authenticateUser, authorizeUser, useCreateForm);
-router.post('/:formId', useCreateFormSubmission);
 
 export default router;
