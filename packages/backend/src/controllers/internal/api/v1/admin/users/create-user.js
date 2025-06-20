b40be9fea50dcfffd88704480b550125d23137ec
creotove <creotove@gmail.com>
2025-06-21 01:06:09 +0530

    fix: User Deletion and User Creation Improvised #2218

--- packages/backend/src/controllers/internal/api/v1/admin/users/create-user.js
+++ packages/backend/src/controllers/internal/api/v1/admin/users/create-user.js
@@ -1,9 +1,42 @@
 import { renderObject } from '@/helpers/renderer.js';
+import { ValidationError } from 'objection';
 import User from '@/models/user.js';
 import Role from '@/models/role.js';
 
 export default async (request, response) => {
-  const user = await User.query().insertAndFetch(await userParams(request));
+  const params = await userParams(request);
+
+  const existingUser = await User.query()
+    .withSoftDeleted()
+    .findOne({ email: params.email });
+
+  if (existingUser) {
+    if (existingUser.deletedAt) {
+      throw new ValidationError({
+        data: {
+          email: [
+            {
+              message: 'A deleted user with this email already exists.',
+            },
+          ],
+        },
+        type: 'ValidationError',
+      });
+    } else {
+      throw new ValidationError({
+        data: {
+          email: [
+            {
+              message: 'This email is already taken.',
+            },
+          ],
+        },
+        type: 'ValidationError',
+      });
+    }
+  }
+
+  const user = await User.query().insertAndFetch(params);
   await user.sendInvitationEmail();
 
   renderObject(response, user, { status: 201, serializer: 'AdminUser' });
