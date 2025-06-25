71f4a6bb79f7ecbb3f93fe4ea6b126afb1c69ed5
Hussain Nagaria <hussainbhaitech@gmail.com>
2025-06-25 13:06:44 +0530

    fix: Use Postgres credentials from .env

--- packages/backend/bin/database/client.js
+++ packages/backend/bin/database/client.js
@@ -1,9 +1,10 @@
 import pg from 'pg';
 
 const client = new pg.Client({
-  host: 'localhost',
-  user: 'postgres',
-  port: 5432,
+  host: process.env.POSTGRES_HOST || 'localhost',
+  user: process.env.POSTGRES_USERNAME || 'postgres',
+  port: Number.parseInt(process.env.POSTGRES_PORT) || 5432,
+  password: process.env.POSTGRES_PASSWORD
 });
 
 export default client;
