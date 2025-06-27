b7bf37f7044fde1dc16b44c5820a21d6b27a5c00
Hussain Nagaria <hussainbhaitech@gmail.com>
2025-06-27 06:23:46 +0530

    fix: Revert changes to database/client.js

--- packages/backend/bin/database/client.js
+++ packages/backend/bin/database/client.js
@@ -1,10 +1,9 @@
 import pg from 'pg';
 
 const client = new pg.Client({
-  host: process.env.POSTGRES_HOST || 'localhost',
-  user: process.env.POSTGRES_USERNAME || 'postgres',
-  port: Number.parseInt(process.env.POSTGRES_PORT) || 5432,
-  password: process.env.POSTGRES_PASSWORD
+  host: 'localhost',
+  user: 'postgres',
+  port: 5432,
 });
 
 export default client;
