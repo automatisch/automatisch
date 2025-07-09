2e1b81166ce8fdfa0fa3a50adfc16660e6b46139
Ali BARIN <ali.barin53@gmail.com>
2025-07-09 17:09:23 +0200

    fix(better-stack/verify-credentials): remove redundant update

--- packages/backend/src/apps/better-stack/auth/verify-credentials.js
+++ packages/backend/src/apps/better-stack/auth/verify-credentials.js
@@ -1,10 +1,5 @@
 const verifyCredentials = async ($) => {
   await $.http.get('/v2/metadata');
-
-  await $.auth.set({
-    screenName: $.auth.data.screenName,
-    apiKey: $.auth.data.apiKey,
-  });
 };
 
 export default verifyCredentials;
