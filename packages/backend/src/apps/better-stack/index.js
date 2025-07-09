a70f2128b407952851c45181b8f85575ea7fb269
Ali BARIN <ali.barin53@gmail.com>
2025-07-09 16:34:12 +0200

    fix(better-stack): use DOCS_URL variable and add missing hash sign in primaryColor

--- packages/backend/src/apps/better-stack/index.js
+++ packages/backend/src/apps/better-stack/index.js
@@ -6,11 +6,11 @@ export default defineApp({
   name: 'Better Stack',
   key: 'better-stack',
   iconUrl: '{BASE_URL}/apps/better-stack/assets/favicon.svg',
-  authDocUrl: 'https://automatisch.io/docs/apps/better-stack/connection',
+  authDocUrl: '{DOCS_URL}/apps/better-stack/connection',
   supportsConnections: true,
   baseUrl: 'https://betterstack.com',
   apiBaseUrl: 'https://uptime.betterstack.com/api',
-  primaryColor: '000000',
+  primaryColor: '#000000',
   beforeRequest: [addAuthHeader],
   auth,
 });
