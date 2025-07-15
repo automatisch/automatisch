9c62ade33cd9edba3e47a6f077dc85c6b87e40cb
Ali BARIN <ali.barin53@gmail.com>
2025-07-15 10:27:42 +0200

    feat(github/triggers): add new pull request event trigger

--- packages/backend/src/apps/github/triggers/index.js
+++ packages/backend/src/apps/github/triggers/index.js
@@ -2,5 +2,12 @@ import newIssues from './new-issues/index.js';
 import newPullRequests from './new-pull-requests/index.js';
 import newStargazers from './new-stargazers/index.js';
 import newWatchers from './new-watchers/index.js';
+import newPullRequestEvent from './new-pull-request-event/index.js';
 
-export default [newIssues, newPullRequests, newStargazers, newWatchers];
+export default [
+  newIssues,
+  newPullRequests,
+  newStargazers,
+  newWatchers,
+  newPullRequestEvent,
+];
