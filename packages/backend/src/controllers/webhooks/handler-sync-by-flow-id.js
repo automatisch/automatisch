980d0d68dacd51963d9042a2a3582a32ab002d70
Faruk AYDIN <omerfaruk26@gmail.com>
2025-07-24 16:59:29 +0200

    refactor: Use executor directly from webhook handlers

--- packages/backend/src/controllers/webhooks/handler-sync-by-flow-id.js
+++ packages/backend/src/controllers/webhooks/handler-sync-by-flow-id.js
@@ -1,29 +1,12 @@
-import Flow from '@/models/flow.js';
-import logger from '@/helpers/logger.js';
-import handlerSync from '@/helpers/webhook-handler-sync.js';
+import { runExecutor } from '@/executor/index.js';
 
 export default async (request, response) => {
-  const computedRequestPayload = {
-    headers: request.headers,
-    body: request.body,
-    query: request.query,
-    params: request.params,
-  };
-
-  logger.debug(`Handling incoming webhook request at ${request.originalUrl}.`);
-  logger.debug(JSON.stringify(computedRequestPayload, null, 2));
-
-  const flowId = request.params.flowId;
-  const flow = await Flow.query().findById(flowId).throwIfNotFound();
-  const triggerStep = await flow.getTriggerStep();
-
-  if (triggerStep.appKey !== 'webhook' && triggerStep.appKey !== 'forms') {
-    const connection = await triggerStep.$relatedQuery('connection');
-
-    if (!(await connection.verifyWebhook(request))) {
-      return response.sendStatus(401);
-    }
-  }
-
-  await handlerSync(flowId, request, response);
+  return await runExecutor({
+    flowId: request.params.flowId,
+    request,
+    response,
+    asyncWebhook: false,
+    syncWebhook: true,
+    triggeredByRequest: true,
+  });
 };
--- packages/backend/src/executor/index.js
+++ packages/backend/src/executor/index.js
@@ -27,11 +27,30 @@ export const runExecutor = async ({
   request,
   response,
 }) => {
+  if (triggeredByRequest) {
+    const computedRequestPayload = {
+      headers: request.headers,
+      body: request.body,
+      query: request.query,
+      params: request.params,
+    };
+
+    logger.debug(
+      `Handling incoming webhook request at ${request.originalUrl}.`
+    );
+    logger.debug(JSON.stringify(computedRequestPayload, null, 2));
+  }
+
   const flow = await Flow.query().findById(flowId);
   let untilStep;
 
   if (!flow) {
     logger.info(`Flow ${flowId} not found!`);
+
+    if (triggeredByRequest) {
+      return response.status(404);
+    }
+
     return;
   }
 
@@ -113,6 +132,80 @@ export const runExecutor = async ({
   }
 
   const { data, error } = $.triggerOutput;
+
+  if (triggeredByRequest && !isBuiltInApp) {
+    if (!(await triggerConnection.verifyWebhook(request))) {
+      return response.sendStatus(401);
+    }
+  }
+
+  if (triggeredByRequest && syncWebhook) {
+    const reversedTriggerItems = $.triggerOutput.data.reverse();
+
+    if (isEmpty(reversedTriggerItems)) {
+      return response.status(204);
+    }
+
+    response.status(204);
+
+    for (const triggerItem of reversedTriggerItems) {
+      const { executionId } = await processTrigger({
+        flowId,
+        stepId: triggerStep.id,
+        triggerItem,
+        testRun,
+      });
+
+      if (testRun) {
+        response.status(204).end();
+
+        // in case of testing, we do not process the whole process.
+        continue;
+      }
+
+      for (const actionStep of actionSteps) {
+        const { executionStep: actionExecutionStep } = await processAction({
+          flowId: flow.id,
+          stepId: actionStep.id,
+          executionId,
+        });
+
+        if (actionStep.appKey === 'filter' && !actionExecutionStep.dataOut) {
+          response.status(422).end();
+
+          break;
+        }
+
+        if (
+          (actionStep.key === 'respondWith' ||
+            actionStep.key === 'respondWithVoiceXml') &&
+          !response.headersSent
+        ) {
+          const { headers, statusCode, body } = actionExecutionStep.dataOut;
+
+          // we set the custom response headers
+          if (headers) {
+            for (const [key, value] of Object.entries(headers)) {
+              if (key) {
+                response.set(key, value);
+              }
+            }
+          }
+
+          // we send the response only if it's not sent yet. This allows us to early respond from the flow.
+          response.status(statusCode);
+          response.send(body);
+        }
+      }
+    }
+
+    // in case the webhook flow does not have any respondWith action. Otherwise, the request will keep hanging.
+    if (!response.headersSent) {
+      response.status(204).end();
+    }
+
+    return response;
+  }
 
   if (triggeredByRequest && asyncWebhook) {
     const reversedTriggerItems = $.triggerOutput.data.reverse();
