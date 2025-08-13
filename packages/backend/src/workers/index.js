c965c4be806da1dd26340fffd0b2320ee410a6a5
Faruk AYDIN <omerfaruk26@gmail.com>
2025-08-13 10:53:14 +0200

    refactor: Use only flow worker and queue

--- packages/backend/src/workers/index.js
+++ packages/backend/src/workers/index.js
@@ -1,16 +1,16 @@
 import appConfig from '@/config/app.js';
-import actionWorker from '@/workers/action.js';
+// import actionWorker from '@/workers/action.js';
 import emailWorker from '@/workers/email.js';
 import flowWorker from '@/workers/flow.js';
-import triggerWorker from '@/workers/trigger.js';
+// import triggerWorker from '@/workers/trigger.js';
 import deleteUserWorker from '@/workers/delete-user.ee.js';
 import removeCancelledSubscriptionsWorker from '@/workers/remove-cancelled-subscriptions.ee.js';
 
 const workers = [
-  actionWorker,
+  // actionWorker,
   emailWorker,
   flowWorker,
-  triggerWorker,
+  // triggerWorker,
   deleteUserWorker,
 ];
 
